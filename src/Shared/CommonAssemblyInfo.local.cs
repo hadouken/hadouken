b54e934641e598555b34839bfd716bdfc091a388
Viktor Elofsson <viktor@viktorelofsson.se>
2012-07-05 18:23:20 +0200

    Made script to setup local build env

---
+++ src/Shared/CommonAssemblyInfo.local.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("Hadouken")]
+
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
