c543fc578c25e8e23177950b354abcca3a687e34
vktr <viktor@viktorelofsson.se>
2012-07-20 12:19:06 +0200

    WebUI progress.

--- src/WebUI/Content/Js/main.js
+++ src/WebUI/Content/Js/main.js
@@ -1,17 +1,14 @@
-window.addEvent("domready", function() {
+window.addEvent("domready", function () {
     setupGlobalEvents();
-    
     setupTorrentListUI();
 
     hdknWebUI.trtTable.setConfig({ "rowMaxCount": Math.ceil(screen.height / 16) || 100, "rowMode": MODE_VIRTUAL });
-    
-    hdknWebUI.trtTable.requiresRefresh = true;
-    hdknWebUI.trtTable.resizePads();
-    hdknWebUI.trtTable.refresh();
-    hdknWebUI.trtTable.calcSize();
-    
+
+    setupToolbar();
+    setupDialogManager();
+
     resizeUI();
-    
+
     /*
     var rdata = tbl.rowData["abcd"];
     console.log(rdata);
@@ -22,7 +19,7 @@ window.addEvent("domready", function() {
     tbl.resizePads();
     tbl.refresh();
     */
-    
+
     hdknWebUI.init();
 });
 
@@ -49,6 +46,96 @@ function setupTorrentListUI() {
         "reverse": false,
         "sIndex": -1
     }));
+}
+
+function setupDialogManager() {
+    //--------------------------------------------------
+    // DIALOG MANAGER
+    //--------------------------------------------------
+
+    DialogManager.init();
+
+    ["About", "Add", "AddEditRSSFeed", "AddURL", "AddLabel", "Props", "RSSDownloader", "Delete"].each(function (k) {
+        var isModal = ["Add", "AddEditRSSFeed", "Props"].contains(k);
+
+        DialogManager.add(k, isModal, {
+            "Add": function () { /* */ }
+            , "AddURL": function () { utWebUI.getDirectoryList(); }
+            , "RSSDownloader": function () { utWebUI.rssDownloaderShow(true); }
+            // , "Settings": function() { utWebUI.stpanes.onChange(); }
+        }[k]);
+    });
+
+}
+
+function setupToolbar() {
+    //--------------------------------------------------
+    // TOOLBAR
+    //--------------------------------------------------
+
+//    utWebUI.updateToolbar();
+
+    // -- Buttons
+
+    ["add", "addurl", "setting"].each(function (act) {
+        $(act).addStopEvent("click", function (ev) {
+            if (ev.target.hasClass("disabled")) {
+                return;
+            }
+
+            var arg;
+            switch (act) {
+                case "add": hdknWebUI.showAddTorrent(); break;
+                case "addurl": utWebUI.showAddURL(); break;
+                case "rssdownloader": utWebUI.showRSSDownloader(); break;
+                case "setting": utWebUI.showSettings(); break;
+
+                case "remove": utWebUI.removeDefault(ev.shift); break;
+
+                case "queueup":
+                case "queuedown":
+                    arg = ev.shift;
+
+                default:
+                    utWebUI[act](arg);
+            }
+        });
+    });
+
+    // -- Toolbar Chevron
+
+    $("tbchevron").addStopEvents({
+        "mousedown": function (ev) {
+            utWebUI.toolbarChevronShow(this);
+        },
+        "click": null
+    });
+
+    // -- Search Field
+
+    $("query").addEvent("keydown", function (ev) {
+        if (ev.key == "enter") {
+            utWebUI.searchExecute();
+        }
+    });
+
+    $("search").addStopEvents({
+        "mousedown": function (ev) {
+            if (ev.isRightClick()) {
+                utWebUI.searchMenuShow(this);
+            }
+        },
+        "click": function (ev) {
+            utWebUI.searchExecute();
+        }
+    });
+
+    $("searchsel").addStopEvents({
+        "mousedown": function (ev) {
+            utWebUI.searchMenuShow(this);
+        },
+        "click": null
+    });
 }
 
 function resizeUI() {
