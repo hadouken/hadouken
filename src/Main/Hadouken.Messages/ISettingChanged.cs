4d70999440ca6678c75095ede5c09633c7edb566
vktr <viktor@viktorelofsson.se>
2012-07-24 11:00:01 +0200

    Added new message, ISettingChanged. Sent whenever /api/config endpoint changes a setting in the database.

---
+++ src/Main/Hadouken.Messages/ISettingChanged.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Hadouken.Messaging;
+
+namespace Hadouken.Messages
+{
+    public interface ISettingChanged : IMessage
+    {
+        string Key { get; set; }
+        string OldValue { get; set; }
+        string NewValue { get; set; }
+    }
+}
