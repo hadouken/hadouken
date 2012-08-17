e61979d02289ec181fdee8c4848277c133ff6e4c
vktr <viktor@viktorelofsson.se>
2012-08-18 01:15:59 +0200

    Fix for when inserting rows with {} in them.

--- src/Main/Hadouken.Impl/Data/DefaultMigratorRunner.cs
+++ src/Main/Hadouken.Impl/Data/DefaultMigratorRunner.cs
@@ -9,6 +9,7 @@ using Hadouken.Data;
 using Migrator.Providers.SQLite;
 using System.IO;
 using Hadouken.Configuration;
+using Migrator.Framework;
 
 namespace Hadouken.Impl.Data
 {
@@ -19,7 +20,7 @@ namespace Hadouken.Impl.Data
             string dataPath = HdknConfig.GetPath("Paths.Data");
             string connectionString = HdknConfig.ConnectionString.Replace("$Paths.Data$", dataPath);
 
-            var m = new Migrator.Migrator(new SQLiteTransformationProvider(new SQLiteDialect(), connectionString), target, false);
+            var m = new Migrator.Migrator(new SQLiteTransformationProvider(new SQLiteDialect(), connectionString), target, false, new FooLogger());
             m.MigrateToLastVersion();
         }
 
@@ -32,4 +33,56 @@ namespace Hadouken.Impl.Data
             m.MigrateTo(-1);
         }
     }
+
+    public class FooLogger : ILogger
+    {
+        public void ApplyingDBChange(string sql)
+        {
+        }
+
+        public void Exception(string message, Exception ex)
+        {
+        }
+
+        public void Exception(long version, string migrationName, Exception ex)
+        {
+        }
+
+        public void Finished(List<long> currentVersion, long finalVersion)
+        {
+        }
+
+        public void Log(string format, params object[] args)
+        {
+        }
+
+        public void MigrateDown(long version, string migrationName)
+        {
+        }
+
+        public void MigrateUp(long version, string migrationName)
+        {
+        }
+
+        public void RollingBack(long originalVersion)
+        {
+        }
+
+        public void Skipping(long version)
+        {
+        }
+
+        public void Started(List<long> currentVersion, long finalVersion)
+        {
+        }
+
+        public void Trace(string format, params object[] args)
+        {
+        }
+
+        public void Warn(string format, params object[] args)
+        {
+        }
+    }
+
 }
