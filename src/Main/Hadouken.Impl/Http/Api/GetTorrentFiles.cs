e665ade42cb632f3e93263052c38954b6af18948
vktr <viktor@viktorelofsson.se>
2012-08-19 12:32:31 +0200

    Two new API actions, one for getting settings and one for getting torrent files.

---
+++ src/Main/Hadouken.Impl/Http/Api/GetTorrentFiles.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Hadouken.Http;
+using Hadouken.BitTorrent;
+
+namespace Hadouken.Impl.Http.Api
+{
+    [ApiAction("gettorrentfiles")]
+    public class GetTorrentFiles : ApiAction
+    {
+        private IBitTorrentEngine _torrentEngine;
+
+        public GetTorrentFiles(IBitTorrentEngine torrentEngine)
+        {
+            _torrentEngine = torrentEngine;
+        }
+
+        public override ActionResult Execute(IHttpContext context)
+        {
+            string hash = context.Request.QueryString["hash"];
+
+            if (!String.IsNullOrEmpty(hash) && _torrentEngine.Managers.ContainsKey(hash))
+            {
+                return Json(new object[]
+                {
+                    (from f in _torrentEngine.Managers[hash].Torrent.Files
+                     select new object[]
+                     {
+                         f.Path,
+                         f.Length,
+                         f.BytesDownloaded,
+                         f.Priority
+                     })
+                });
+            }
+
+            // no hash not specified or no torrent with specified hash
+            // return empty array
+
+            return Json(new object[] { });
+        }
+    }
+}
