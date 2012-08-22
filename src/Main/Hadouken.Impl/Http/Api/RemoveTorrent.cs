561cc9d2bd306d0b7498ff24a8c2c3b851dc78f7
vktr <viktor@viktorelofsson.se>
2012-08-22 20:27:08 +0200

    Added (untested) new API actions stop, pause and remove.

---
+++ src/Main/Hadouken.Impl/Http/Api/RemoveTorrent.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Hadouken.Http;
+using Hadouken.BitTorrent;
+
+namespace Hadouken.Impl.Http.Api
+{
+    [ApiAction("remove")]
+    public class RemoveTorrent : ApiAction
+    {
+        private IBitTorrentEngine _torrentEngine;
+
+        public RemoveTorrent(IBitTorrentEngine torrentEngine)
+        {
+            _torrentEngine = torrentEngine;
+        }
+
+        public override ActionResult Execute()
+        {
+            string[] hashes = BindModel<string[]>();
+
+            foreach (var hash in hashes)
+            {
+                if (_torrentEngine.Managers.ContainsKey(hash))
+                    _torrentEngine.RemoveTorrent(_torrentEngine.Managers[hash]);
+            }
+
+            return Json(true);
+        }
+    }
+}
