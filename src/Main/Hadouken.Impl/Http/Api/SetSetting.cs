5022df11ab12231937eeecd4a891637df3e0c487
vktr <viktor@viktorelofsson.se>
2012-09-23 13:51:55 +0200

    Implemented `setsetting` API action.

---
+++ src/Main/Hadouken.Impl/Http/Api/SetSetting.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Hadouken.Http;
+using Hadouken.Configuration;
+
+namespace Hadouken.Impl.Http.Api
+{
+    [ApiAction("setsetting")]
+    public class SetSetting : ApiAction
+    {
+        private IKeyValueStore _keyValueStore;
+
+        public SetSetting(IKeyValueStore keyValueStore)
+        {
+            this._keyValueStore = keyValueStore;
+        }
+
+        public override ActionResult Execute()
+        {
+            var data = BindModel<Dictionary<string, object>>();
+
+            foreach (var key in data.Keys)
+            {
+                _keyValueStore.Set(key, data[key]);
+            }
+
+            return Json(true);
+        }
+    }
+}
