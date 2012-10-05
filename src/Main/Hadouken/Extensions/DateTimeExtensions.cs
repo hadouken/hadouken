b44757c6263c668954c245274818a584b67c3785
vktr <viktor@viktorelofsson.se>
2012-10-06 01:19:08 +0200

    Changed variable name to `UnixTime`.

--- src/Main/Hadouken/Extensions/DateTimeExtensions.cs
+++ src/Main/Hadouken/Extensions/DateTimeExtensions.cs
@@ -7,11 +7,11 @@ namespace Hadouken
 {
     public static class DateTimeExtensions
     {
-        private static DateTime unix = new DateTime(1970, 1, 1).ToLocalTime();
+        private static readonly DateTime UnixTime = new DateTime(1970, 1, 1).ToLocalTime();
 
         public static int ToUnixTime(this DateTime dt)
         {
-            return (int)(dt - unix).TotalSeconds;
+            return (int)(dt - UnixTime).TotalSeconds;
         }
     }
 }
