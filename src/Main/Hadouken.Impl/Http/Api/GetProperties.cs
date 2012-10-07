132771409c25cb55beb068f2071cb10b38e02e8d
vktr <viktor@viktorelofsson.se>
2012-10-07 15:45:12 +0200

    Improved `getprops`

--- src/Main/Hadouken.Impl/Http/Api/GetProperties.cs
+++ src/Main/Hadouken.Impl/Http/Api/GetProperties.cs
@@ -21,17 +21,29 @@ namespace Hadouken.Impl.Http.Api
         {
             string hash = Context.Request.QueryString["hash"];
 
-            if(_torrentEngine.Managers.ContainsKey(hash))
+            if (_torrentEngine.Managers.ContainsKey(hash))
             {
                 var manager = _torrentEngine.Managers[hash];
-                return Json(new[]
+                return Json(new
                                 {
-                                    new
-                                        {
-                                            hash = manager.InfoHash,
-                                            trackers = "",
-                                            //ulrate = manager.Settings
-                                        }
+                                    props = new[]
+                                                {
+                                                    new
+                                                        {
+                                                            hash = manager.InfoHash,
+                                                            trackers = "",
+                                                            ulrate = manager.Settings.MaxUploadSpeed,
+                                                            dlrate = manager.Settings.MaxDownloadSpeed,
+                                                            superseed = manager.IsInitialSeeding,
+                                                            dht = manager.Settings.UseDht,
+                                                            pex = manager.Settings.EnablePeerExchange,
+                                                            seed_override = false,
+                                                            seed_ratio = 0,
+                                                            seed_time = 0,
+                                                            ulslots = manager.Settings.UploadSlots,
+                                                            seed_num = 0
+                                                        }
+                                                }
                                 });
             }
 
