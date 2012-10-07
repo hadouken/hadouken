88848ecae60478646ff2f8257ce9a0399b9c9649
vktr <viktor@viktorelofsson.se>
2012-10-07 14:49:06 +0200

    Added new API action `getsysteminfo`.

---
+++ src/Main/Hadouken.Impl/Http/Api/GetSystemInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Hadouken.Http;
+
+namespace Hadouken.Impl.Http.Api
+{
+    [ApiAction("getsysteminfo")]
+    public class GetSystemInfo : ApiAction
+    {
+        public override ActionResult Execute()
+        {
+            return Json(new
+                            {
+                                version = typeof (Kernel).Assembly.GetName().Version
+                            });
+        }
+    }
+}
