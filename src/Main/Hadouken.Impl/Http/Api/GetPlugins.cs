dbdffd6e2b09a87eb160819fa1efa9cc46009936
vktr <viktor@viktorelofsson.se>
2012-10-07 16:01:30 +0200

    Added API action `getplugins`.

---
+++ src/Main/Hadouken.Impl/Http/Api/GetPlugins.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Hadouken.Http;
+using Hadouken.Plugins;
+
+namespace Hadouken.Impl.Http.Api
+{
+    [ApiAction("getplugins")]
+    public class GetPlugins : ApiAction
+    {
+        private readonly IPluginEngine _pluginEngine;
+
+        public GetPlugins(IPluginEngine pluginEngine)
+        {
+            _pluginEngine = pluginEngine;
+        }
+
+        public override ActionResult Execute()
+        {
+            return Json(new
+                            {
+                                plugins = (from m in _pluginEngine.Managers.Values
+                                           select new
+                                                      {
+                                                          name = m.Name.ToLowerInvariant(),
+                                                          version = m.Version.ToString(),
+                                                          init = "boot.js" // should not hardcode this
+                                                      }).ToArray()
+                            });
+        }
+    }
+}
