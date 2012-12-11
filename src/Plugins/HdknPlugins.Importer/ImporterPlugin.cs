6f8f12369d6d08819aaf5f8743d3e69efc32296f
Viktor Elofsson <viktor@viktorelofsson.se>
2012-12-11 23:35:22 +0100

    Importer plugin

---
+++ src/Plugins/HdknPlugins.Importer/ImporterPlugin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Hadouken.Plugins;
+
+namespace HdknPlugins.Importer
+{
+    [Plugin("importer", "1.0", ResourceBase = "HdknPlugins.Importer.UI")]
+    public class ImporterPlugin : IPlugin
+    {
+        public void Load()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Unload()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
