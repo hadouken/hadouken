66619f6cb43024b0a0d5430a37b3d2dcb0995e4d
Viktor Elofsson <viktor@viktorelofsson.se>
2013-03-14 11:01:35 +0100

    Rake zip task also refactored.

--- tools/buildscripts/test.rb
+++ tools/buildscripts/test.rb
@@ -1,24 +1,21 @@
 ﻿namespace :test do
-    task :x86 => "build:x86" do
-        puts "##teamcity[progressMessage 'Running unit tests for x86 code']"
+    def run_tests(platform)
+        puts "##teamcity[progressMessage 'Running unit tests for #{platform} code']"
         FileUtils.mkdir_p "build/reports" unless FileTest.exists?("build/reports")
         
         if(ENV['TEAMCITY_VERSION'])
-            system "#{ENV['NUNIT_LAUNCHER']} v4.0 x86 NUnit-2.6.0 src/Tests/Hadouken.UnitTests/bin/x86/#{CONFIGURATION}/Hadouken.UnitTests.dll"
+            system "#{ENV['NUNIT_LAUNCHER']} v4.0 #{platform} NUnit-2.6.0 src/Tests/Hadouken.UnitTests/bin/#{platform}/#{CONFIGURATION}/Hadouken.UnitTests.dll"
         else
-            system "tools/nunit-2.6.0.12051/bin/nunit-console-x86.exe /framework:v4.0.30319 /xml:build/reports/nunit-x86.xml src/Tests/Hadouken.UnitTests/bin/x86/#{CONFIGURATION}/Hadouken.UnitTests.dll"
+            system "tools/nunit-2.6.0.12051/bin/nunit-console#{platform == :x86 ? "-x86" : ""}.exe /framework:v4.0.30319 /xml:build/reports/nunit-#{platform}.xml src/Tests/Hadouken.UnitTests/bin/#{platform}/#{CONFIGURATION}/Hadouken.UnitTests.dll"
         end
+    end
+
+    task :x86 => "build:x86" do
+        run_tests(:x86)
     end
     
     task :x64 => "build:x64" do
-        puts "##teamcity[progressMessage 'Running unit tests for x86 code']"
-        FileUtils.mkdir_p "build/reports" unless FileTest.exists?("build/reports")
-        
-        if(ENV['TEAMCITY_VERSION'])
-            system "#{ENV['NUNIT_LAUNCHER']} v4.0 x64 NUnit-2.6.0 src/Tests/Hadouken.UnitTests/bin/x64/#{CONFIGURATION}/Hadouken.UnitTests.dll"
-        else
-            system "tools/nunit-2.6.0.12051/bin/nunit-console.exe /framework:v4.0.30319 /xml:build/reports/nunit-x64.xml src/Tests/Hadouken.UnitTests/bin/x64/#{CONFIGURATION}/Hadouken.UnitTests.dll"
-        end
+        run_tests(:x64)
     end
     
     task :all do
