2809ceee9c9b5acedad4d4b340990365c59d0339
Viktor Elofsson <viktor@viktorelofsson.se>
2013-03-14 21:16:57 +0100

    First working version of the release flow.

--- tools/buildscripts/release.rb
+++ tools/buildscripts/release.rb
@@ -1,5 +1,9 @@
 require 'semver'
+require 'psych'
 require 'httparty'
+require 'net/sftp'
+
+CFG = YAML.load_file("config/config.yml") unless defined? CFG
 
 namespace :release do
   task :common do
@@ -45,8 +49,11 @@ namespace :release do
     ensure_msi_packages(version.format("%M.%m.%p"))
     ensure_version_available(version.to_s)
     
+    copy_to_server("build/msi/hdkn-#{version.format("%M.%m.%p")}.4000-x86.msi", version.format("%M.%m"))
+    copy_to_server("build/msi/hdkn-#{version.format("%M.%m.%p")}.4000-x64.msi", version.format("%M.%m"))
+    
     tag_repo(version.to_s)
-    push_repo(:origin, version.to_s)
+    push_repo(CFG["git"]["remote"], version.to_s)
   end
   
   def ensure_msi_packages(v)
@@ -65,11 +72,13 @@ namespace :release do
   end
   
   def ensure_correct_branch()
-    puts "checking current branch (must be master)"
+    release_branch = CFG["git"]["release_branch"]
+    
+    puts "checking current branch (must be #{release_branch})"
     branch = `git rev-parse --abbrev-ref HEAD`.strip
     
-    if branch != "releaseflow" then
-      fail "can only release from master branch (current branch is #{branch})"
+    if branch != release_branch then
+      fail "can only release from branch #{release_branch} (current branch is #{branch})"
     end
   end
   
@@ -86,7 +95,7 @@ namespace :release do
   def ensure_version_available(v)
     puts "checking with github if we have released #{v} already"
     
-    response = HTTParty.get("https://api.github.com/repos/hadouken/hdkn/tags")
+    response = HTTParty.get(CFG["github"]["api"]["tags"])
     
     response.each do |tag|
       tv = version(tag["name"])
@@ -108,6 +117,31 @@ namespace :release do
     puts "version OK"
   end
   
+  def copy_to_server(localFile, v)
+    puts "copying file #{localFile} to remote server"
+    
+    s = CFG["sftp"]
+    
+    Net::SFTP.start(s["host"], s["user"], :keys => s["keys"]) do |sftp|
+      hasDir = false
+      
+      sftp.dir.foreach(s["path"]) do |p|
+        if(p.name == v)
+          puts "directory #{File.join(s["path"], v)} already exists"
+          hasDir = true
+        end
+      end
+      
+      unless hasDir
+        puts "creating remote directory #{File.join(s["path"], v)}"
+        sftp.mkdir!(File.join(s["path"], v))
+      end
+      
+      puts "uploading file"
+      sftp.upload!(localFile, File.join(s["path"], v, File.basename(localFile)))
+    end
+  end
+  
   def tag_repo(v)
     puts "tagging repo with version #{v}"
     `git tag #{v}`
@@ -115,6 +149,7 @@ namespace :release do
   
   def push_repo(remote, tag)
     puts "pushing tag #{tag} to remote repository #{remote}"
-    `git push test #{tag}`
+    `git push #{remote}`
+    `git push #{remote} #{tag}`
   end
 end
