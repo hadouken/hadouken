7c496c263d77a81ff45e8b8bd92806326922654d
Viktor Elofsson <viktor@viktorelofsson.se>
2013-04-02 22:07:42 +0200

    Added beginning of new IPluginEngine which will load each plugin in its own separate AppDomain.

---
+++ src/Main/Hadouken.Plugins.PluginEngine/DefaultPluginEngine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hadouken.Plugins.PluginEngine
+{
+    public class DefaultPluginEngine : IPluginEngine
+    {
+        public void Load()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Load(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UnloadAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDictionary<string, IPluginManager> Managers
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
