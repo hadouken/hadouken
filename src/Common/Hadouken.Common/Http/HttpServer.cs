e78ff1f894de89323f1fa8196f6dba1c38a2865f
Viktor Elofsson <viktor@viktorelofsson.se>
2013-04-04 16:27:18 +0200

    Added base for shared HttpServer.

---
+++ src/Common/Hadouken.Common/Http/HttpServer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace Hadouken.Common.Http
+{
+    public abstract class HttpServer
+    {
+        private Action<IHttpContext> _requestCallback;
+
+        public abstract void Start(Uri binding, NetworkCredential credentials);
+        public abstract void Stop();
+
+        public void SetRequestCallback(Action<IHttpContext> requestCallback)
+        {
+            if(requestCallback == null)
+                throw new ArgumentNullException("requestCallback");
+
+            _requestCallback = requestCallback;
+        }
+
+        protected void OnHttpRequest(IHttpContext context)
+        {
+            if (_requestCallback != null)
+                _requestCallback(context);
+        }
+    }
+}
