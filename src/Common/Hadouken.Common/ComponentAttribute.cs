fa329a7c5bb8fbcf82588070c50a6a0f4a4fe1ff
Viktor Elofsson <viktor@viktorelofsson.se>
2013-04-05 11:08:27 +0200

    Added a Component attribute which is used to mark DI/IoC components.

---
+++ src/Common/Hadouken.Common/ComponentAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hadouken.Common
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class ComponentAttribute : Attribute
+    {
+        public ComponentAttribute()
+        {
+        }
+
+        /// <summary>
+        /// Uses the specified instance of IComponentFactory to create this component.
+        /// </summary>
+        /// <param name="factory">The factory type, must inherit IComponentFactory.</param>
+        public ComponentAttribute(Type factory)
+        {
+            Factory = factory;
+        }
+
+        public Type Factory { get; private set; }
+    }
+}
