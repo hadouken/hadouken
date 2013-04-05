04c853cf6c78cd011feaf6fb0ff599ffe929947e
Viktor Elofsson <viktor@viktorelofsson.se>
2013-04-05 20:54:27 +0200

    Added a factory that creates IHttpServers.

---
+++ src/Common/Hadouken.Common.Http.HttpListener/HttpListenerServerFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hadouken.Common.Http.HttpListener
+{
+    [Component]
+    public class HttpListenerServerFactory : IHttpServerFactory
+    {
+        public IHttpServer Create(string binding, System.Net.NetworkCredential credential)
+        {
+            return new HttpListenerServer(new Uri(binding), credential);
+        }
+    }
+}
