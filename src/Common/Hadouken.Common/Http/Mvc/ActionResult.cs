bd9aff66a0076955aaf05172cac4c5ac17d9da2e
Viktor Elofsson <viktor@viktorelofsson.se>
2013-04-05 10:29:06 +0200

    Support classes for MVC style classes.

---
+++ src/Common/Hadouken.Common/Http/Mvc/ActionResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hadouken.Common.Http.Mvc
+{
+    public abstract class ActionResult
+    {
+        public abstract void Execute(IHttpContext context);
+    }
+}
---
+++ src/Common/Hadouken.Common/Http/Mvc/JsonResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hadouken.Common.Http.Mvc
+{
+    public class JsonResult : ActionResult
+    {
+        private readonly object _object;
+
+        public JsonResult(object obj)
+        {
+            _object = obj;
+        }
+
+        public override void Execute(IHttpContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
