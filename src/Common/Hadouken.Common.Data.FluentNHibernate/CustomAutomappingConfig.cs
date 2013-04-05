11eb09ce053c980f0d03f8369d3e84ef371fbcca
Viktor Elofsson <viktor@viktorelofsson.se>
2013-04-05 16:16:16 +0200

    Added a new common data access project using FluentNHibernate. Should replace the one in Impl.

---
+++ src/Common/Hadouken.Common.Data.FluentNHibernate/CustomAutomappingConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hadouken.Common.Data.FluentNHibernate
+{
+    public class CustomAutomappingConfig
+    {
+        public override bool ShouldMap(Type type)
+        {
+            return typeof (Model).IsAssignableFrom(type);
+        }
+    }
+}
---
+++ src/Common/Hadouken.Common.Data.FluentNHibernate/FluentNHibernateDataRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hadouken.Common.Data.FluentNHibernate
+{
+    public class FluentNHibernateDataRepository : IDataRepository
+    {
+        public void Save<TModel>(TModel model) where TModel : Model, new()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveOrUpdate<TModel>(TModel model) where TModel : Model, new()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update<TModel>(TModel model) where TModel : Model, new()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete<TModel>(TModel model) where TModel : Model, new()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TModel Single<TModel>(int id) where TModel : Model, new()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TModel Single<TModel>(System.Linq.Expressions.Expression<Func<TModel, bool>> query) where TModel : Model, new()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TModel> List<TModel>() where TModel : Model, new()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TModel> List<TModel>(System.Linq.Expressions.Expression<Func<TModel, bool>> query) where TModel : Model, new()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
