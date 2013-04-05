e74f889f8bb6d33e4bbac9c76bccc688cc6c05f3
Viktor Elofsson <viktor@viktorelofsson.se>
2013-04-05 10:28:50 +0200

    Documented the extension method for the message bus.

--- src/Common/Hadouken.Common/Messaging/MessageBusExtensions.cs
+++ src/Common/Hadouken.Common/Messaging/MessageBusExtensions.cs
@@ -8,6 +8,13 @@ namespace Hadouken.Common.Messaging
 {
     public static class MessageBusExtensions
     {
+        /// <summary>
+        /// Sends a message on the bus and waits for a response to this message of the specified message type.
+        /// </summary>
+        /// <typeparam name="TMessage">A class that inherits Message.</typeparam>
+        /// <param name="messageBus"></param>
+        /// <param name="outgoing">The message to send.</param>
+        /// <returns>The message which is a response to the message sent.</returns>
         public static TMessage Call<TMessage>(this IMessageBus messageBus, Message outgoing)
             where TMessage : Message
         {
