452dca36f5f009c67d49763a0bd2e700858486b3
Viktor Elofsson <viktor@viktorelofsson.se>
2013-04-05 20:54:02 +0200

    Fixed the FluentNHibernate repository.

--- src/Common/Hadouken.Common.Data.FluentNHibernate/FluentNHibernateDataRepository.cs
+++ src/Common/Hadouken.Common.Data.FluentNHibernate/FluentNHibernateDataRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using NHibernate;
+using NHibernate.Linq;
 using FluentNHibernate.Cfg;
 using FluentNHibernate.Cfg.Db;
 
@@ -12,6 +13,7 @@ namespace Hadouken.Common.Data.FluentNHibernate
     public class FluentNHibernateDataRepository : IDataRepository
     {
         private ISessionFactory _sessionFactory;
+        private ISession _session;
 
         public FluentNHibernateDataRepository()
         {
@@ -21,48 +23,54 @@ namespace Hadouken.Common.Data.FluentNHibernate
         private void BuildSessionFactory()
         {
             _sessionFactory = Fluently.Configure()
-                                      .Database(SQLiteConfiguration.Standard.ConnectionString(""))
+                                      .Database(SQLiteConfiguration.Standard.ConnectionString("Data Source=:memory:"))
                                       .BuildSessionFactory();
+
+            _session = _sessionFactory.OpenSession();
         }
 
         public void Save<TModel>(TModel model) where TModel : Model, new()
         {
-            throw new NotImplementedException();
+            _session.Save(model);
+            _session.Flush();
         }
 
         public void SaveOrUpdate<TModel>(TModel model) where TModel : Model, new()
         {
-            throw new NotImplementedException();
+            _session.SaveOrUpdate(model);
+            _session.Flush();
         }
 
         public void Update<TModel>(TModel model) where TModel : Model, new()
         {
-            throw new NotImplementedException();
+            _session.Update(model);
+            _session.Flush();
         }
 
         public void Delete<TModel>(TModel model) where TModel : Model, new()
         {
-            throw new NotImplementedException();
+            _session.Delete(model);
+            _session.Flush();
         }
 
         public TModel Single<TModel>(int id) where TModel : Model, new()
         {
-            throw new NotImplementedException();
+            return _session.Get<TModel>(id);
         }
 
         public TModel Single<TModel>(System.Linq.Expressions.Expression<Func<TModel, bool>> query) where TModel : Model, new()
         {
-            throw new NotImplementedException();
+            return _session.Query<TModel>().Where(query).SingleOrDefault();
         }
 
         public IEnumerable<TModel> List<TModel>() where TModel : Model, new()
         {
-            throw new NotImplementedException();
+            return _session.Query<TModel>().ToList();
         }
 
         public IEnumerable<TModel> List<TModel>(System.Linq.Expressions.Expression<Func<TModel, bool>> query) where TModel : Model, new()
         {
-            throw new NotImplementedException();
+            return _session.Query<TModel>().Where(query).ToList();
         }
     }
 }
