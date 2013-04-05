2b235ff0e11a78af4b08afcec86302197546597a
Viktor Elofsson <viktor@viktorelofsson.se>
2013-04-05 20:55:11 +0200

    Added a factory that creates IHttpServers.

--- src/Common/Hadouken.Common/Http/IHttpServerFactory.cs
+++ src/Common/Hadouken.Common/Http/IHttpServerFactory.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 
 namespace Hadouken.Common.Http
 {
     public interface IHttpServerFactory
     {
-        IHttpServer Create();
+        IHttpServer Create(string binding, NetworkCredential credential);
     }
 }
