ccc663d13b67e6f41c17eb2e18bf6900068ad0cf
Viktor Elofsson <viktor@viktorelofsson.se>
2013-04-05 16:25:08 +0200

    Began implementing the data repository.

--- src/Common/Hadouken.Common.Data.FluentNHibernate/FluentNHibernateDataRepository.cs
+++ src/Common/Hadouken.Common.Data.FluentNHibernate/FluentNHibernateDataRepository.cs
@@ -2,11 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NHibernate;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
 
 namespace Hadouken.Common.Data.FluentNHibernate
 {
     public class FluentNHibernateDataRepository : IDataRepository
     {
+        private ISessionFactory _sessionFactory;
+
+        public FluentNHibernateDataRepository()
+        {
+            BuildSessionFactory();
+        }
+
+        private void BuildSessionFactory()
+        {
+            _sessionFactory = Fluently.Configure()
+                                      .Database(SQLiteConfiguration.Standard.ConnectionString(""))
+                                      .BuildSessionFactory();
+        }
+
         public void Save<TModel>(TModel model) where TModel : Model, new()
         {
             throw new NotImplementedException();
