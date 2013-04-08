3d7e6462dfeb93f49574bc4ee06c89958c21c5e8
Viktor Elofsson <viktor@viktorelofsson.se>
2013-04-08 19:53:21 +0200

    Added IFileSystem to AutoAdd plugin. Obvious.

--- src/Plugins/HdknPlugins.AutoAdd/AutoAddPlugin.cs
+++ src/Plugins/HdknPlugins.AutoAdd/AutoAddPlugin.cs
@@ -8,18 +8,23 @@ using Hadouken.Common.Data;
 
 using HdknPlugins.AutoAdd.Timers;
 using HdknPlugins.AutoAdd.Data.Models;
+using Hadouken.Common.IO;
+using Hadouken.Common.BitTorrent;
 
 namespace HdknPlugins.AutoAdd
 {
     public class AutoAddPlugin : Plugin
     {
         private readonly IDataRepository _dataRepository;
+        private readonly IFileSystem _fileSystem;
         private readonly ITimer _timer;
 
-        public AutoAddPlugin(IMessageBus messageBus, IDataRepository dataRepository, ITimerFactory timerFactory)
+        public AutoAddPlugin(IMessageBus messageBus, IDataRepository dataRepository, IFileSystem fileSystem,
+                             ITimerFactory timerFactory)
             : base(messageBus)
         {
             _dataRepository = dataRepository;
+            _fileSystem = fileSystem;
             _timer = timerFactory.CreateTimer();
         }
 
