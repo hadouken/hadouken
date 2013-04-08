1b61ba10bc371ee452e4b1af3641d8ed2a6a352f
Viktor Elofsson <viktor@viktorelofsson.se>
2013-04-08 19:34:01 +0200

    Added a message handler which will add torrents to the IBitTorrentEngine implementation.

---
+++ src/Main/Hadouken/BitTorrent/MessageHandlers/AddTorrentHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Hadouken.Common.Messaging;
+using Hadouken.Common.BitTorrent;
+
+namespace Hadouken.BitTorrent.MessageHandlers
+{
+    public class AddTorrentHandler : IMessageHandler<AddTorrentMessage>
+    {
+        private readonly IBitTorrentEngine _torrentEngine;
+
+        public AddTorrentHandler(IBitTorrentEngine torrentEngine)
+        {
+            _torrentEngine = torrentEngine;
+        }
+
+        public void Handle(AddTorrentMessage message)
+        {
+            var manager = _torrentEngine.AddTorrent(message.Data);
+
+            if (manager != null && !String.IsNullOrEmpty(message.Label))
+            {
+                manager.Label = message.Label;
+            }
+        }
+    }
+}
