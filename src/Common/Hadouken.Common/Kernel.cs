fcd8c40f2710c75f9af9df83decfa3364f26cd86
Viktor Elofsson <viktor@viktorelofsson.se>
2013-04-08 13:14:36 +0200

    Added a static Kernel class for locating components outside of constructors. Wishing I can remove it later on.

---
+++ src/Common/Hadouken.Common/Kernel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hadouken.Common
+{
+    public static class Kernel
+    {
+        public static T Get<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static object Get(Type type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IEnumerable<T> GetAll<T>()
+        {
+            throw new NotImplementedException();
+        } 
+
+        public static IEnumerable<object> GetAll(Type type)
+        {
+            throw new NotImplementedException();
+        } 
+    }
+}
