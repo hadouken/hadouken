79b63c0a17bddb75b26234885cc52f3da4f9f05f
Viktor Elofsson <viktor@viktorelofsson.se>
2013-04-08 19:25:20 +0200

    Added new message for adding torrents.

---
+++ src/Common/Hadouken.Common/BitTorrent/AddTorrentMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Hadouken.Common.Messaging;
+
+namespace Hadouken.Common.BitTorrent
+{
+    public class AddTorrentMessage : Message
+    {
+        public byte[] Data { get; set; }
+
+        public string Label { get; set; }
+    }
+}
