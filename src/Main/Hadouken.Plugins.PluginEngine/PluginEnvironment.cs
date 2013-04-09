ee75fd2b3ea6e7b6c2c2264d0e124f97517a1c8b
Viktor Elofsson <viktor@viktorelofsson.se>
2013-04-09 21:44:13 +0200

    Added a PluginEnvironment.

---
+++ src/Main/Hadouken.Plugins.PluginEngine/PluginEnvironment.cs
@@ -0,0 +1,13 @@
+﻿using Hadouken.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hadouken.Plugins.PluginEngine
+{
+    public class PluginEnvironment : IEnvironment
+    {
+        public string ConnectionString { get; set; }
+    }
+}
--- src/Main/Hadouken.Plugins.PluginEngine/PluginSandbox.cs
+++ src/Main/Hadouken.Plugins.PluginEngine/PluginSandbox.cs
@@ -1,17 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using Hadouken.Common;
 using Hadouken.Common.Plugins;
 using Hadouken.Common.Messaging;
 using Hadouken.Common.DI;
+using Hadouken.Common.IO;
 
 namespace Hadouken.Plugins.PluginEngine
 {
     public class PluginSandbox : MarshalByRefObject
     {
         private Plugin _plugin;
+
+        public PluginSandbox()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+        }
+
+        System.Reflection.Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            return (from asm in AppDomain.CurrentDomain.GetAssemblies()
+                    where asm.FullName == args.Name
+                    select asm).FirstOrDefault();
+        }
 
         public void AddAssemblies(IEnumerable<byte[]> assemblies)
         {
@@ -36,6 +50,19 @@ namespace Hadouken.Plugins.PluginEngine
                 {
                     var factory = Kernel.Get<IMessageBusFactory>();
                     return factory.Create("hdkn.plugins." + manifest.Name.ToLowerInvariant());
+                });
+
+            Kernel.BindToFunc<IEnvironment>(() =>
+                {
+                    var databasePath = System.IO.Path.Combine(ConfigurationManager.AppSettings["Paths.Data"],
+                                                              String.Format("hdkn.plugins.{0}.db", manifest.Name));
+
+                    var env = new PluginEnvironment()
+                        {
+                            ConnectionString = String.Format("Data Source={0};", databasePath)
+                        };
+
+                    return env;
                 });
 
             _plugin = Kernel.Get<Plugin>();
