8173cbebc495e2e29f2f7005efd8add3f17836ac
Viktor Elofsson <viktor@viktorelofsson.se>
2013-04-09 20:31:00 +0200

    Added an IEnvironment interface to get away from static classes with configuration parameters. Should also be used in plugin environments.

---
+++ src/Main/Hadouken.Impl/HostEnvironment.cs
@@ -0,0 +1,14 @@
+﻿using Hadouken.Common;
+using Hadouken.Configuration;
+
+namespace Hadouken.Impl
+{
+    [Component]
+    public class HostEnvironment : IEnvironment
+    {
+        public string ConnectionString
+        {
+            get { return HdknConfig.ConnectionString; }
+        }
+    }
+}
