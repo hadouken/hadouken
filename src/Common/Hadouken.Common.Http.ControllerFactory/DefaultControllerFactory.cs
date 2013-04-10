4bb77f2627ba166c5dcb19e3720dfc08b5247462
Viktor Elofsson <viktor@viktorelofsson.se>
2013-04-10 17:21:52 +0200

    Added default IControllerFactory base.

---
+++ src/Common/Hadouken.Common.Http.ControllerFactory/DefaultControllerFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Hadouken.Common.Http.Mvc;
+
+namespace Hadouken.Common.Http.ControllerFactory
+{
+    public class DefaultControllerFactory : IControllerFactory
+    {
+        private readonly IEnvironment _environment;
+        private readonly IController[] _controllers;
+
+        public DefaultControllerFactory(IEnvironment environment, IController[] controllers)
+        {
+            _environment = environment;
+            _controllers = controllers;
+        }
+
+        public void Execute(IHttpContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
