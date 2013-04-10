4733fff0771c89352fa3f1309afbdd61db851e60
Viktor Elofsson <viktor@viktorelofsson.se>
2013-04-10 12:18:04 +0200

    Fixes to the AutoAddPlugin.

--- src/Plugins/HdknPlugins.AutoAdd/AutoAddPlugin.cs
+++ src/Plugins/HdknPlugins.AutoAdd/AutoAddPlugin.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Hadouken.Common;
 using Hadouken.Common.Plugins;
 using Hadouken.Common.Messaging;
 using Hadouken.Common.Data;
@@ -10,19 +11,29 @@ using HdknPlugins.AutoAdd.Timers;
 using HdknPlugins.AutoAdd.Data.Models;
 using Hadouken.Common.IO;
 using Hadouken.Common.BitTorrent;
+using Migrator.Providers.SQLite;
+using NLog;
+using System.Text.RegularExpressions;
 
 namespace HdknPlugins.AutoAdd
 {
     public class AutoAddPlugin : Plugin
     {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        private readonly IEnvironment _environment;
         private readonly IDataRepository _dataRepository;
         private readonly IFileSystem _fileSystem;
         private readonly ITimer _timer;
 
-        public AutoAddPlugin(IMessageBus messageBus, IDataRepository dataRepository, IFileSystem fileSystem,
+        public AutoAddPlugin(IEnvironment environment,
+                             IMessageBus messageBus,
+                             IDataRepository dataRepository,
+                             IFileSystem fileSystem,
                              ITimerFactory timerFactory)
             : base(messageBus)
         {
+            _environment = environment;
             _dataRepository = dataRepository;
             _fileSystem = fileSystem;
             _timer = timerFactory.CreateTimer();
@@ -30,17 +41,82 @@ namespace HdknPlugins.AutoAdd
 
         public override void Load()
         {
-            _timer.SetCallback(1000, CheckFolders);
+            Logger.Trace("Load()");
+
+            var m =
+                new Migrator.Migrator(
+                    new SQLiteTransformationProvider(new SQLiteDialect(), _environment.ConnectionString),
+                    this.GetType().Assembly, false);
+
+            Logger.Debug("Updating all migrations in current assembly");
+
+            m.MigrateToLastVersion();
+
+            _timer.SetCallback(5000, CheckFolders);
             _timer.Start();
         }
 
         private void CheckFolders()
         {
-            //var folders = _dataRepository.List<Folder>();
+            var folders = _dataRepository.List<Folder>();
+
+            foreach (var folder in folders)
+            {
+                CheckFolder(folder);
+            }
+        }
+
+        internal void CheckFolder(Folder folder)
+        {
+            var files = _fileSystem.GetFiles(folder.Path, "*.torrent");
+
+            foreach (var file in files)
+            {
+                string fileName = System.IO.Path.GetFileName(file);
+
+                if (String.IsNullOrEmpty(fileName))
+                    continue;
+
+                if (!String.IsNullOrEmpty(folder.IncludeFilter) || !String.IsNullOrEmpty(folder.ExcludeFilter))
+                {
+                    bool include =
+                        !(!String.IsNullOrEmpty(folder.IncludeFilter) && !Regex.IsMatch(fileName, folder.IncludeFilter));
+
+                    bool exclude =
+                        !(!String.IsNullOrEmpty(folder.ExcludeFilter) && !Regex.IsMatch(fileName, folder.ExcludeFilter));
+
+                    if (include && exclude)
+                    {
+                        AddFile(file, folder.Label, folder.AutoStart);
+                    }
+                }
+                else
+                {
+                    AddFile(file, folder.Label, folder.AutoStart);
+                }
+            }
+        }
+
+        internal void AddFile(string file, string label, bool autoStart)
+        {
+            var data = _fileSystem.ReadAllBytes(file);
+
+            var msg = new AddTorrentMessage
+                {
+                    AutoStart = autoStart,
+                    Data = data,
+                    Label = label
+                };
+
+            MessageBus.Publish(msg);
+
+            _fileSystem.DeleteFile(file);
         }
 
         public override void Unload()
         {
+            Logger.Trace("Unload()");
+
             _timer.Stop();
         }
     }
