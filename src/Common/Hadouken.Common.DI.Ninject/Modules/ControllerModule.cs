cbb80448b3af73815446155c5c4609cfcd6b4c60
Viktor Elofsson <viktor@viktorelofsson.se>
2013-04-10 17:17:49 +0200

    Added Ninject module to register all IController's.

---
+++ src/Common/Hadouken.Common.DI.Ninject/Modules/ControllerModule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Ninject.Extensions.Conventions;
+using Ninject.Modules;
+using Hadouken.Common.Http.Mvc;
+
+namespace Hadouken.Common.DI.Ninject.Modules
+{
+    public class ControllerModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Kernel.Bind(ctx => ctx.From(AppDomain.CurrentDomain.GetAssemblies())
+                                  .SelectAllClasses()
+                                  .InheritedFrom<IController>()
+                                  .BindAllInterfaces());
+        }
+    }
+}
