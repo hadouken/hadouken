a3b7568da90da5f6a41c7f436522dcb9cf7d79c2
Viktor Elofsson <viktor@viktorelofsson.se>
2013-04-11 16:45:04 +0200

    Added some new ActionResults and a FileLocationType.

---
+++ src/Common/Hadouken.Common/Http/FileLocationType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hadouken.Common.Http
+{
+    public enum FileLocationType
+    {
+        FileSystem,
+        EmbeddedResource
+    }
+}
