03875c71153146d61c2d6a996ea4913fc18adaec
Viktor Elofsson <viktor@viktorelofsson.se>
2013-04-26 11:51:58 +0200

    Fixed the BitTorrentEngineTests.

--- src/Tests/Hadouken.UnitTests/BitTorrent/BitTorrentEngineTests.cs
+++ src/Tests/Hadouken.UnitTests/BitTorrent/BitTorrentEngineTests.cs
@@ -15,6 +15,7 @@ namespace Hadouken.UnitTests.BitTorrent
     {
         private readonly Mock<IFileSystem> fs = new Mock<IFileSystem>();    
         private readonly Mock<IMessageBus> bus = new Mock<IMessageBus>();
+        private readonly Mock<IMessageBusFactory> _factory = new Mock<IMessageBusFactory>();
         private readonly Mock<IDataRepository> repo = new Mock<IDataRepository>();
         private readonly Mock<IKeyValueStore> kvs = new Mock<IKeyValueStore>();
         private MonoTorrentEngine engine;
@@ -22,13 +23,14 @@ namespace Hadouken.UnitTests.BitTorrent
         [TestFixtureSetUp]
         public void TestFixtureSetUp()
         {
-            HdknConfig.ConfigManager = new MemoryConfigManager();            
+            HdknConfig.ConfigManager = new MemoryConfigManager();
+            _factory.Setup(f => f.Create(It.IsAny<string>())).Returns(bus.Object);
         }
 
         [SetUp]
         public void SetUp()
         {
-            //engine = new MonoTorrentEngine(fs.Object, bus.Object, repo.Object, kvs.Object);
+            engine = new MonoTorrentEngine(fs.Object, _factory.Object, repo.Object, kvs.Object);
         }
 
         [TearDown]
