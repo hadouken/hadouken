e9a19c0ef849aeeb25eb34cf2053871074c4c173
Viktor Elofsson <viktor@viktorelofsson.se>
2013-05-11 16:35:57 +0200

    Added Ninject modules for registering components.

---
+++ src/Main/Hadouken.DI.Ninject/Modules/ApiActionModule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Hadouken.Reflection;
+using Ninject.Modules;
+using Hadouken.Http;
+
+namespace Hadouken.DI.Ninject.Modules
+{
+    public class ApiActionModule : NinjectModule
+    {
+        public override void Load()
+        {
+            var apiActionTypes = (from asm in AppDomain.CurrentDomain.GetAssemblies()
+                                  from type in asm.GetTypes()
+                                  where typeof (IApiAction).IsAssignableFrom(type)
+                                  where type.IsClass && !type.IsAbstract
+                                  select type);
+
+            foreach (var apiActionType in apiActionTypes)
+            {
+                Kernel.Bind<IApiAction>().To(apiActionType);
+            }
+        }
+    }
+}
---
+++ src/Main/Hadouken.DI.Ninject/Modules/ComponentModule.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Hadouken.Reflection;
+using Ninject.Modules;
+
+namespace Hadouken.DI.Ninject.Modules
+{
+    public class ComponentModule : NinjectModule
+    {
+        public override void Load()
+        {
+            var componentTypes = (from asm in AppDomain.CurrentDomain.GetAssemblies()
+                                  from type in asm.GetTypes()
+                                  where type.HasAttribute<ComponentAttribute>()
+                                  where type.IsClass && !type.IsAbstract
+                                  select type);
+
+            foreach (var componentType in componentTypes)
+            {
+                var attr = componentType.GetAttribute<ComponentAttribute>();
+                var style = (attr == null ? ComponentLifestyle.Singleton : attr.Lifestyle);
+                var binding = Kernel.Bind(componentType.GetInterfaces()).To(componentType);
+
+                switch (style)
+                {
+                    case ComponentLifestyle.Singleton:
+                        binding.InTransientScope();
+                        break;
+
+                    case ComponentLifestyle.Transient:
+                        binding.InSingletonScope();
+                        break;
+                }
+            }
+        }
+    }
+}
