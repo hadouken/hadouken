95128f5cc2eeb59e217e7773934d9dc859a934d1
Viktor Elofsson <viktor@viktorelofsson.se>
2013-06-28 16:13:12 +0200

    Implemented the SetCredentials API action.

--- src/Main/Hadouken/Http/Api/SetCredentials.cs
+++ src/Main/Hadouken/Http/Api/SetCredentials.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Hadouken.Configuration;
+using Hadouken.Security;
 
 namespace Hadouken.Http.Api
 {
@@ -19,7 +20,29 @@ namespace Hadouken.Http.Api
 
         public override ActionResult Execute()
         {
-            throw new NotImplementedException();
+            var model = BindModel<Dictionary<string, string>>();
+
+            if (!model.ContainsKey("oldPassword"))
+                return Json(false);
+
+            var oldPassword = Hash.Generate(model["oldPassword"]);
+            var currentPassword = _keyValueStore.Get<string>("auth.password");
+
+            if (!String.Equals(oldPassword, currentPassword, StringComparison.InvariantCultureIgnoreCase))
+                return Json(false);
+
+            if (model.ContainsKey("username"))
+            {
+                _keyValueStore.Set("auth.username", model["username"]);
+            }
+
+            if (model.ContainsKey("password"))
+            {
+                var hashedPassword = Hash.Generate(model["password"]);
+                _keyValueStore.Set("auth.password", hashedPassword);
+            }
+
+            return Json(true);
         }
     }
 }
