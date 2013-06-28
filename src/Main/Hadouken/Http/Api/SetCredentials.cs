ff1f0dd2b1d278f8f0c2875aed728f1d07db82e6
Viktor Elofsson <viktor@viktorelofsson.se>
2013-06-28 16:01:15 +0200

    Added new API action to change credentials.

---
+++ src/Main/Hadouken/Http/Api/SetCredentials.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hadouken.Configuration;
+
+namespace Hadouken.Http.Api
+{
+    [ApiAction("setcredentials")]
+    public class SetCredentials : ApiAction
+    {
+        private readonly IKeyValueStore _keyValueStore;
+
+        public SetCredentials(IKeyValueStore keyValueStore)
+        {
+            _keyValueStore = keyValueStore;
+        }
+
+        public override ActionResult Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
