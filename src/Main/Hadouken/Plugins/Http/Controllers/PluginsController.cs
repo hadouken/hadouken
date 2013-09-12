e9d23213738b2e6de1f5cb1c363d20b02a2f31d9
Viktor Elofsson <viktor@viktorelofsson.se>
2013-09-12 18:15:35 +0200

    Added the PluginsController which should handle all plugin operations.

---
+++ src/Main/Hadouken/Plugins/Http/Controllers/PluginsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Hadouken.Plugins.Http.Models;
+
+namespace Hadouken.Plugins.Http.Controllers
+{
+    public class PluginsController : ApiController
+    {
+        private readonly IPluginEngine _pluginEngine;
+
+        public PluginsController(IPluginEngine pluginEngine)
+        {
+            _pluginEngine = pluginEngine;
+        }
+
+        public HttpResponseMessage Get()
+        {
+            var dtos = (from plugin in _pluginEngine.GetAll()
+                select new PluginListDto
+                {
+                    Name = plugin.Name
+                }).ToList();
+
+            return Request.CreateResponse(HttpStatusCode.OK, dtos);
+        }
+    }
+}
---
+++ src/Main/Hadouken/Plugins/Http/Models/PluginListDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hadouken.Plugins.Http.Models
+{
+    public class PluginListDto
+    {
+        public string Name { get; set; }
+    }
+}
