bc76e37fa360c2a18b95c828ed1d2e66369bb76a
Viktor Elofsson <viktor@viktorelofsson.se>
2013-09-15 14:52:57 +0200

    Missing file in Events plugin.

---
+++ src/Plugins/Events/Hadouken.Plugin.Events/Rpc/EventsService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hadouken.Framework.Rpc;
+using Hadouken.Plugins.Events.Hubs;
+
+namespace Hadouken.Plugins.Events.Rpc
+{
+    public class EventsService : IJsonRpcService
+    {
+        private readonly IEventHub _eventHub;
+
+        public EventsService(IEventHub eventHub)
+        {
+            _eventHub = eventHub;
+        }
+
+        [JsonRpcMethod("events.publish")]
+        public bool Publish(string name, object data)
+        {
+            _eventHub.Publish(name, data);
+            return true;
+        }
+    }
+}
