bff01926b41092d99a48801d8312f32c00e3ee32
Viktor Elofsson <viktor@viktorelofsson.se>
2013-09-15 15:19:50 +0200

    WebUI plugin now shows a Hello World page.

---
+++ src/Plugins/WebUI/Hadouken.Plugins.WebUI/WebUIBootstrapper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hadouken.Framework;
+using Hadouken.Framework.Http;
+using Hadouken.Framework.Plugins;
+
+namespace Hadouken.Plugins.WebUI
+{
+    public class WebUIBootstrapper : Bootstrapper
+    {
+        public override Plugin Load(IBootConfig config)
+        {
+            var uri = String.Format("http://{0}:{1}/", config.HostBinding, config.Port);
+            var server = new HttpFileServer(uri, Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "UI"));
+
+            return new WebUIPlugin(server);
+        }
+    }
+}
--- src/Plugins/WebUI/Hadouken.Plugins.WebUI/WebUIPlugin.cs
+++ src/Plugins/WebUI/Hadouken.Plugins.WebUI/WebUIPlugin.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Hadouken.Framework.Http;
 using Hadouken.Framework.Plugins;
 
 namespace Hadouken.Plugins.WebUI
 {
     public class WebUIPlugin : Plugin
     {
+        private readonly IHttpFileServer _fileServer;
+
+        public WebUIPlugin(IHttpFileServer fileServer)
+        {
+            _fileServer = fileServer;
+        }
+
         public override void Load()
         {
-            throw new NotImplementedException();
+            _fileServer.Open();
+        }
+
+        public override void Unload()
+        {
+            _fileServer.Close();
         }
     }
 }
