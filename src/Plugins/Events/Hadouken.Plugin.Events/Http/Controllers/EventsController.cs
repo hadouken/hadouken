3de6582fb73dbdccb58212fa5e0e6dd94657c1d1
Viktor Elofsson <viktor@viktorelofsson.se>
2013-09-15 10:48:12 +0200

    Began working on the self-hosted WebAPI in Events plugin.

---
+++ src/Plugins/Events/Hadouken.Plugin.Events/Http/Controllers/EventsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Hadouken.Plugins.Events.Http.Models;
+using Hadouken.Plugins.Events.Hubs;
+
+namespace Hadouken.Plugins.Events.Http.Controllers
+{
+    public class EventsController : ApiController
+    {
+        private readonly IEventHub _eventHub;
+
+        public EventsController(IEventHub eventHub)
+        {
+            _eventHub = eventHub;
+        }
+
+        public HttpResponseMessage Post(PostEventDto dto)
+        {
+            if (dto == null)
+                return new HttpResponseMessage(HttpStatusCode.BadRequest);
+
+            if (String.IsNullOrEmpty(dto.Name) || dto.Data == null)
+                return new HttpResponseMessage(HttpStatusCode.BadRequest);
+
+            _eventHub.Publish(dto.Name, dto.Data);
+
+            return new HttpResponseMessage(HttpStatusCode.NoContent);
+        }
+    }
+}
