35a0e5fdb85ba11e496823c4ffaefa24fdeca45a
Viktor Elofsson <viktor@viktorelofsson.se>
2013-09-15 14:53:18 +0200

    Initial commit of the WebUI plugin.

---
+++ src/Plugins/WebUI/Hadouken.Plugins.WebUI/WebUIPlugin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hadouken.Framework.Plugins;
+
+namespace Hadouken.Plugins.WebUI
+{
+    public class WebUIPlugin : Plugin
+    {
+        public override void Load()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
