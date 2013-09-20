498e65c0be849278a0125214014b078daa8f0e0c
Viktor Elofsson <viktor@viktorelofsson.se>
2013-09-20 23:16:41 +0200

    Added JsonRpcClient for javascript.

--- src/Plugins/WebUI/Hadouken.Plugins.WebUI/UI/Scripts/Framework/Hadouken.js
+++ src/Plugins/WebUI/Hadouken.Plugins.WebUI/UI/Scripts/Framework/Hadouken.js
@@ -1,4 +1,11 @@
 ﻿var Hadouken = {
-    init: function() {
+    init: function () {
+        var rpcClient = new JsonRpcClient();
+        
+        rpcClient.call("plugins.list", null, function(pluginList) {
+            for (var i = 0; i < pluginList.length; i++) {
+                console.log(pluginList[i].name);
+            }
+        });
     }
 };
