5fdb387cb72b92db680b27dd6fea564be0db4d3a
Viktor Elofsson <viktor@viktorelofsson.se>
2013-09-21 11:58:07 +0200

    Added base for Torrents plugin.

---
+++ src/Plugins/Torrents/Hadouken.Plugins.Torrents/TorrentsBootstrapper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using Hadouken.Framework;
+using Hadouken.Framework.Plugins;
+
+namespace Hadouken.Plugins.Torrents
+{
+    public class TorrentsBootstrapper : Bootstrapper
+    {
+        public override Plugin Load(IBootConfig config)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Plugins/Torrents/Hadouken.Plugins.Torrents/TorrentsPlugin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hadouken.Framework.Plugins;
+
+namespace Hadouken.Plugins.Torrents
+{
+    public class TorrentsPlugin : Plugin
+    {
+        public override void Load()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
