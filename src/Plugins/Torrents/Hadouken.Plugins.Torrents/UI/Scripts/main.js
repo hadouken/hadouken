57228d2297ba0dbd49446f7099d9a507033ddc8e
Viktor Elofsson <viktor@viktorelofsson.se>
2013-09-22 12:02:53 +0200

    MenuItem and Page classes working together in symphony.

--- src/Plugins/Torrents/Hadouken.Plugins.Torrents/UI/Scripts/main.js
+++ src/Plugins/Torrents/Hadouken.Plugins.Torrents/UI/Scripts/main.js
@@ -1,1 +1,31 @@
 ﻿console.log("Loading Torrents plugin");
+
+var TorrentsListPage = new Class({
+    Extends: Page,
+    
+    initialize: function() {
+        this.parent("/plugins/core.torrents/list.html");
+    },
+
+    load: function() {
+        console.log("TorrentsListPage::load()");
+    },
+    
+    unload: function() {
+        console.log("TorrentsListPage::unload()");
+    }
+});
+
+try {
+
+    MainMenu.addItem(
+        new MenuItem("Torrents", {
+            onClick: function() {
+                Pages.navigateTo(new TorrentsListPage());
+            }
+        })
+    );
+
+} catch(e) {
+    console.log(e);
+}
---
+++ src/Plugins/WebUI/Hadouken.Plugins.WebUI/UI/Scripts/Framework/Page.js
@@ -0,0 +1,46 @@
+﻿var Page = new Class({
+    url: '',
+    html: '',
+    
+    initialize: function(url) {
+        this.url = url;
+    },
+    
+    preload: function(callback) {
+        new Request({            
+            url: this.url,
+            method: 'get',
+            onSuccess: function(html) {
+                this.html = html;
+                callback();
+            }.bind(this)
+        }).send();
+    },
+    
+    render: function(target) {
+        $(target).empty();
+        $(target).set('html', this.html);
+    },
+    
+    load: Function.from(),
+    
+    unload: Function.from()
+});
+
+var Pages = {
+    currentPage: null,
+    
+    navigateTo: function(page) {
+        if (this.currentPage != null) {
+            this.currentPage.unload();
+            this.currentPage = null;
+        }
+
+        this.currentPage = page;
+
+        this.currentPage.preload(function() {
+            this.currentPage.render('page-container');
+            this.currentPage.load();
+        }.bind(this));
+    }
+};
