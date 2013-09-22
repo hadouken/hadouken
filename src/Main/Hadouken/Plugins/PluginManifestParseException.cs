79924e2a8792cf7d58cd7890129099e69bf5b327
Viktor Elofsson <viktor@viktorelofsson.se>
2013-09-22 20:11:53 +0200

    Added some custom exceptions.

---
+++ src/Main/Hadouken/Plugins/PluginManifestParseException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hadouken.Plugins
+{
+    internal class PluginManifestParseException : Exception
+    {
+        public PluginManifestParseException(Exception innerException)
+            : base("Could not load plugin manifest file", innerException)
+        {
+        }
+    }
+}
