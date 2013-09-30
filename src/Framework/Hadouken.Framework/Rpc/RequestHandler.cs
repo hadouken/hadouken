cdd73445cbf63b709c357d59b31327e341e9f4c6
Viktor Elofsson <viktor@viktorelofsson.se>
2013-09-30 17:04:49 +0200

    Abstracting away the request handlng.

---
+++ src/Framework/Hadouken.Framework/Rpc/RequestHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hadouken.Framework.Rpc
+{
+    public class RequestHandler : IRequestHandler
+    {
+        public JsonRpcResponse Execute(JsonRpcRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
