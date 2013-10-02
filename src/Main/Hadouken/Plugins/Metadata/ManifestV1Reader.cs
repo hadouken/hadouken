72a0db70a6a8e72dfccde746df48814ad9387dd3
Viktor Elofsson <viktor@viktorelofsson.se>
2013-10-02 10:59:41 +0200

    Removed the ManifestParser class.
    Added a ManifestV1Reader to parse v1 manifests.

---
+++ src/Main/Hadouken/Plugins/Metadata/ManifestV1Reader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hadouken.Plugins.Metadata
+{
+    public class ManifestV1Reader : IManifestReader
+    {
+        public Manifest Read(Newtonsoft.Json.Linq.JObject manifestObject)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
