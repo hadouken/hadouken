92fb88550ec8a13f7ff3c3f3bdb1972ed3f37b21
Viktor Elofsson <viktor@viktorelofsson.se>
2013-10-02 11:00:51 +0200

    Manifest class now has a static TryParse to parse manifest files.

--- src/Main/Hadouken/Plugins/Metadata/Manifest.cs
+++ src/Main/Hadouken/Plugins/Metadata/Manifest.cs
@@ -1,17 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Hadouken.Plugins.Metadata
 {
-    public sealed class Manifest
+    public sealed class Manifest : IManifest
     {
+        private static readonly IDictionary<int, Func<IManifestReader>> ManifestReaders =
+            new Dictionary<int, Func<IManifestReader>>();
+
+        static Manifest()
+        {
+            ManifestReaders.Add(1, () => new ManifestV1Reader());
+        }
+
         public string Name { get; set; }
 
-        public Version Version { get; set; }
+        public SemanticVersion Version { get; set; }
 
         public Dependency[] Dependencies { get; set; }
+
+        public static bool TryParse(Stream json, out IManifest manifest)
+        {
+            Exception exception;
+            return TryParse(json, out manifest, out exception);
+        }
+
+        public static bool TryParse(Stream json, out IManifest manifest, out Exception exception)
+        {
+            manifest = null;
+            exception = null;
+
+            try
+            {
+                using (var streamReader = new StreamReader(json))
+                using (var jsonReader = new JsonTextReader(streamReader))
+                {
+                    var obj = JToken.ReadFrom(jsonReader) as JObject;
+
+                    if (obj == null)
+                        return false;
+
+                    JToken manifestVersionToken;
+
+                    if (!obj.TryGetValue("manifest_version", out manifestVersionToken))
+                        return false;
+
+                    if (manifestVersionToken.Type != JTokenType.Integer)
+                        return false;
+
+                    var manifestVersion = manifestVersionToken.Value<int>();
+
+                    if (!ManifestReaders.ContainsKey(manifestVersion))
+                        return false;
+
+                    var manifestReader = ManifestReaders[manifestVersion]();
+                    manifest = manifestReader.Read(obj);
+
+                    return true;
+                }
+            }
+            catch (Exception e)
+            {
+                exception = e;
+                return false;
+            }
+        }
     }
 }
--- src/Main/Hadouken/Plugins/Metadata/SemanticVersion.cs
+++ src/Main/Hadouken/Plugins/Metadata/SemanticVersion.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Hadouken.Plugins.Metadata
 {
-    class SemanticVersion
+    public class SemanticVersion
     {
     }
 }
