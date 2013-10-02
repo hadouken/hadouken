8bc464feed0b9fd2604ff74ee3c147e7da2905b4
Viktor Elofsson <viktor@viktorelofsson.se>
2013-10-02 12:40:00 +0200

    SemanticVersion class now working almost as expected. With tests to prove it.

---
+++ src/Main/Hadouken.Tests/Plugins/Metadata/SemanticVersionTests.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hadouken.Plugins.Metadata;
+using NUnit.Framework;
+
+namespace Hadouken.Tests.Plugins.Metadata
+{
+    public class SemanticVersionTests
+    {
+        [Test]
+        public void CanConstructSemanticVersionFromDefault()
+        {
+            var semver = new SemanticVersion();
+            Assert.AreEqual("0.0.0", semver.ToString());
+        }
+
+        [Test]
+        public void CanConstructSemanticVersionFromMajor()
+        {
+            var semver = new SemanticVersion(1);
+            Assert.AreEqual("1.0.0", semver.ToString());
+        }
+
+        [Test]
+        public void CanConstructSemanticVersionFromMajorMinor()
+        {
+            var semver = new SemanticVersion(1, 1);
+            Assert.AreEqual("1.1.0", semver.ToString());
+        }
+
+        [Test]
+        public void CanConstructSemanticVersionFromMajorMinorPatch()
+        {
+            var semver = new SemanticVersion(1, 1, 1);
+            Assert.AreEqual("1.1.1", semver.ToString());
+        }
+
+        [Test]
+        public void CanConstructSemanticVersionFromMajorMinorPatchLabel()
+        {
+            var semver = new SemanticVersion(1, 1, 1, "label");
+            Assert.AreEqual("1.1.1-label", semver.ToString());
+        }
+
+        [Test]
+        public void CanConstructSemanticVersionFromString()
+        {
+            var semver = new SemanticVersion("1.0.1-label");
+            Assert.AreEqual("1.0.1-label", semver.ToString());
+        }
+
+        [Test]
+        public void LessThanComparisonWithMajorMinorSucceeds()
+        {
+            var lower = new SemanticVersion("1.0");
+            var upper = new SemanticVersion("1.1");
+
+            Assert.IsTrue(lower < upper);
+        }
+
+        [Test]
+        public void LessThanComparisonWithMajorMinorPatchSucceeds()
+        {
+            var lower = new SemanticVersion("1.1.132");
+            var upper = new SemanticVersion("1.1.133");
+
+            Assert.IsTrue(lower < upper);
+        }
+
+        [Test]
+        public void LessThanComparisonWithMajorMinorPatchLabelSucceeds()
+        {
+            var lower = new SemanticVersion("1.1.123-label.1");
+            var upper = new SemanticVersion("1.1.123-label.2");
+
+            Assert.IsTrue(lower < upper);
+        }
+
+        [Test]
+        public void ImplicitStringOperatorSucceeds()
+        {
+            SemanticVersion semver = "1.2.3-label";
+            Assert.AreEqual("1.2.3-label", semver.ToString());
+        }
+
+        [Test]
+        public void SetsCorrectLabel()
+        {
+            var semver = new SemanticVersion("1.9.0-sweet.label");
+            Assert.AreEqual("sweet.label", semver.Label);
+        }
+    }
+}
--- src/Main/Hadouken/Plugins/Metadata/SemanticVersion.cs
+++ src/Main/Hadouken/Plugins/Metadata/SemanticVersion.cs
@@ -15,6 +15,11 @@ namespace Hadouken.Plugins.Metadata
 
         public SemanticVersion() : this(0, 0, 0, String.Empty) {}
 
+        public SemanticVersion(string semver)
+        {
+            ParseFromString(semver);
+        }
+
         public SemanticVersion(int major) : this(major, 0, 0, String.Empty) {}
 
         public SemanticVersion(int major, int minor) : this(major, minor, 0, String.Empty) {}
@@ -60,6 +65,99 @@ namespace Hadouken.Plugins.Metadata
         }
 
         public string Label { get; set; }
+
+        public static bool operator <(SemanticVersion x, SemanticVersion y)
+        {
+            if (x == null)
+                throw new ArgumentNullException("x");
+
+            if (y == null)
+                throw new ArgumentNullException("y");
+
+            if (x.Major < y.Major)
+                return true;
+
+            if (x.Minor < y.Minor)
+                return true;
+
+            if (x.Patch < y.Patch)
+                return true;
+
+            return String.Compare(x.Label, y.Label, StringComparison.Ordinal) < 0;
+        }
+
+        public static bool operator >(SemanticVersion x, SemanticVersion y)
+        {
+            if (x == null)
+                throw new ArgumentNullException("x");
+
+            if (y == null)
+                throw new ArgumentNullException("y");
+
+            if (x.Major > y.Major)
+                return true;
+
+            if (x.Minor > y.Minor)
+                return true;
+
+            if (x.Patch > y.Patch)
+                return true;
+
+            return String.Compare(x.Label, y.Label, StringComparison.Ordinal) > 0;
+        }
+
+        public static implicit operator SemanticVersion(string semver)
+        {
+            return new SemanticVersion(semver);
+        }
+
+        public static implicit operator string(SemanticVersion semver)
+        {
+            if (semver == null)
+                throw new ArgumentNullException("semver");
+
+            return semver.ToString();
+        }
+
+        public override string ToString()
+        {
+            if (String.IsNullOrEmpty(Label))
+                return String.Format("{0}.{1}.{2}", Major, Minor, Patch);
+
+            return String.Format("{0}.{1}.{2}-{3}", Major, Minor, Patch, Label);
+        }
+
+        private void ParseFromString(string semver)
+        {
+            if (String.IsNullOrEmpty(semver))
+                throw new ArgumentNullException("semver");
+
+            var label = String.Empty;
+
+            if (semver.Contains("-"))
+            {
+                label = semver.Substring(semver.IndexOf("-", StringComparison.Ordinal) + 1);
+                semver = semver.Substring(0, semver.IndexOf("-", StringComparison.Ordinal));
+            }
+
+            var parts = semver.Split(new[] {'.'}, StringSplitOptions.RemoveEmptyEntries);
+
+            if (parts.Length < 1 || parts.Length > 3)
+                throw new ArgumentException("Invalid semantic version.", "semver");
+
+            if (parts[0].Contains("-"))
+                throw new ArgumentException("Invalid semantic version", "semver");
+
+            Major = Convert.ToInt32(parts[0]);
+
+            if (parts.Length > 1)
+                Minor = Convert.ToInt32(parts[1]);
+
+            if (parts.Length > 2)
+                Patch = Convert.ToInt32(parts[2]);
+
+            Label = label;
+        }
 
         private static void ThrowIfNegative(int value)
         {
