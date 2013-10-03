1291652bbb04685c03230b487f65d4737ce811fc
Viktor Elofsson <viktor@viktorelofsson.se>
2013-10-03 11:29:47 +0200

    Moved Rule classes to their own files.

---
+++ src/Main/Hadouken/Plugins/Metadata/LessThanRule.cs
@@ -0,0 +1,17 @@
+﻿namespace Hadouken.Plugins.Metadata
+{
+    public class LessThanRule : Rule
+    {
+        private readonly SemanticVersion _version;
+
+        public LessThanRule(SemanticVersion version)
+        {
+            _version = version;
+        }
+
+        public override bool IsIncluded(SemanticVersion version)
+        {
+            return version < _version;
+        }
+    }
+}
---
+++ src/Main/Hadouken/Plugins/Metadata/Rule.cs
@@ -0,0 +1,7 @@
+﻿namespace Hadouken.Plugins.Metadata
+{
+    public abstract class Rule
+    {
+        public abstract bool IsIncluded(SemanticVersion version);
+    }
+}
