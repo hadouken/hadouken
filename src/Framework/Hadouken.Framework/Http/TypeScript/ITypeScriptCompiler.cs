67035db1a13b26d90786a3fdb7e0ed4bb0846c8a
Viktor Elofsson <viktor@viktorelofsson.se>
2013-10-04 10:56:18 +0200

    Super simple TypeScriptCompiler implementation.

--- src/Framework/Hadouken.Framework/Http/TypeScript/ITypeScriptCompiler.cs
+++ src/Framework/Hadouken.Framework/Http/TypeScript/ITypeScriptCompiler.cs
@@ -3,10 +3,10 @@
     public interface ITypeScriptCompiler
     {
         /// <summary>
-        /// Compiles the TypeScript file and returns the content of the output.
+        /// Compiles the TypeScript file and returns the path to the compiled file.
         /// </summary>
         /// <param name="file">The TypeScript file to compile.</param>
-        /// <returns>The content of the compiled TypeScript file.</returns>
+        /// <returns>The path to the compiled TypeScript file.</returns>
         string Compile(string file);
     }
 }
---
+++ src/Framework/Hadouken.Framework/Http/TypeScript/TypeScriptCompiler.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+
+namespace Hadouken.Framework.Http.TypeScript
+{
+    public class TypeScriptCompiler : ITypeScriptCompiler
+    {
+        private readonly string _toolsPath;
+
+        protected TypeScriptCompiler(string toolsPath)
+        {
+            _toolsPath = toolsPath;
+        }
+
+        public string Compile(string file)
+        {
+            var inputFileDirectory = Path.GetDirectoryName(file);
+
+            if (String.IsNullOrEmpty(inputFileDirectory))
+                return null;
+
+            var fileName = Path.GetFileNameWithoutExtension(file) + ".js";
+
+            var p = new Process();
+            p.StartInfo.Arguments = String.Format("{0} --out {1}", file, Path.Combine(inputFileDirectory, fileName));
+            p.StartInfo.WorkingDirectory = _toolsPath;
+            p.StartInfo.FileName = "tsc.exe";
+
+            p.Start();
+            p.WaitForExit();
+
+            return Path.Combine(inputFileDirectory, fileName);
+        }
+
+        public static ITypeScriptCompiler Create()
+        {
+            var resourceBase = "Hadouken.Framework.Http.TypeScript.Resources.";
+            var temporaryPath = Path.Combine(Path.GetTempPath(), "tsc");
+
+            if (!Directory.Exists(temporaryPath))
+                Directory.CreateDirectory(temporaryPath);
+
+            var assembly = typeof (TypeScriptCompiler).Assembly;
+
+            // Extract resources to temporary folder
+            foreach (var resourceName in assembly.GetManifestResourceNames())
+            {
+                if (!resourceName.StartsWith(resourceBase))
+                    continue;
+
+                var outputName = resourceName.Substring(resourceBase.Length);
+
+                using (var resourceStream = assembly.GetManifestResourceStream(resourceName))
+                using (var outputStream = new StreamWriter(Path.Combine(temporaryPath, outputName)))
+                {
+                    if (resourceStream == null)
+                        continue;
+
+                    resourceStream.CopyTo(outputStream.BaseStream);
+                }
+            }
+
+            return new TypeScriptCompiler(temporaryPath);
+        }
+    }
+}
