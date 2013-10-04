ed99fad8ce9909efe1fb04e56d9b5cad3ab08ea9
Viktor Elofsson <viktor@viktorelofsson.se>
2013-10-04 10:34:20 +0200

    Added an ITypeScriptCompiler interface and the resources needed for compilation.

---
+++ src/Framework/Hadouken.Framework/Http/TypeScript/ITypeScriptCompiler.cs
@@ -0,0 +1,12 @@
+﻿namespace Hadouken.Framework.Http.TypeScript
+{
+    public interface ITypeScriptCompiler
+    {
+        /// <summary>
+        /// Compiles the TypeScript file and returns the content of the output.
+        /// </summary>
+        /// <param name="file">The TypeScript file to compile.</param>
+        /// <returns>The content of the compiled TypeScript file.</returns>
+        string Compile(string file);
+    }
+}
