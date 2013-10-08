22c23994b7dd75afed7114c3ac57f00b4c2e70e5
Viktor Elofsson <viktor@viktorelofsson.se>
2013-10-08 19:46:26 +0200

    JsonRpcClient now uses the same serializer settings as the other parts.

--- src/Framework/Hadouken.Framework/Rpc/JsonRpcClient.cs
+++ src/Framework/Hadouken.Framework/Rpc/JsonRpcClient.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
@@ -6,14 +7,24 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Serialization;
 
 namespace Hadouken.Framework.Rpc
 {
     public sealed class JsonRpcClient : IDisposable
     {
+        private static readonly JsonSerializerSettings SerializerSettings = new JsonSerializerSettings();
         private readonly Uri _host;
         private readonly HttpClient _httpClient = new HttpClient();
         private int _requestId = 0;
+
+        static JsonRpcClient()
+        {
+            SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            SerializerSettings.Converters.Add(new SemanticVersionConverter());
+            SerializerSettings.Converters.Add(new StringEnumConverter());
+            SerializerSettings.Converters.Add(new VersionConverter());
+        }
 
         public JsonRpcClient(Uri host)
         {
@@ -32,7 +43,7 @@ namespace Hadouken.Framework.Rpc
                     @params = parameters
                 };
 
-            var json = JsonConvert.SerializeObject(request);
+            var json = JsonConvert.SerializeObject(request, SerializerSettings);
             var response = await _httpClient.PostAsync(_host, new StringContent(json, Encoding.UTF8, "application/json"));
             var responseContent = await response.Content.ReadAsStringAsync();
 
