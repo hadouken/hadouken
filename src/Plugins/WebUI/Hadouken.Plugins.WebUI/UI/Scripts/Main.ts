c69ee483c3ab07baf2a3fbf81d41d4cb31da2c5d
Viktor Elofsson <viktor@viktorelofsson.se>
2013-10-08 18:50:54 +0200

    Removed unused code.

--- src/Plugins/WebUI/Hadouken.Plugins.WebUI/UI/Scripts/Main.ts
+++ src/Plugins/WebUI/Hadouken.Plugins.WebUI/UI/Scripts/Main.ts
@@ -15,29 +15,3 @@ $(document).ready(function () {
     new Hadouken.Bootstrapper()
         .init(eventListener, pluginEngine, pageManager);
 });
-
-/*
-// Real setup below
-window.addEvent('domready', function () {
-    $("overlay").show();
-
-    Hadouken.init();
-
-    var host = location.hostname;
-    var port = parseInt(location.port) + 1;
-
-    var signalrConnection = jQuery.hubConnection('http://' + host + ':' + port);
-    var eventsProxy = signalrConnection.createHubProxy('events');
-
-    eventsProxy.on('publishEvent', function(event) {
-        if (event.name == "plugin.loaded" || event.name == "plugin.unloaded")
-            location.reload();
-
-        console.log(event.name);
-    });
-
-    signalrConnection.start().done(function() { console.log('connected') });
-
-    $("overlay").hide();
-});
-*/
