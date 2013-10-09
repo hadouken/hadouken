4af9a009727e9f28575f7f2f9077839fcf1ec75a
Viktor Elofsson <viktor@viktorelofsson.se>
2013-10-09 14:54:01 +0200

    Simplified the settings page.

--- src/Plugins/WebUI/Hadouken.Plugins.WebUI/UI/Scripts/Framework/UI/Pages/SettingsPage.ts
+++ src/Plugins/WebUI/Hadouken.Plugins.WebUI/UI/Scripts/Framework/UI/Pages/SettingsPage.ts
@@ -7,74 +7,59 @@ module Hadouken.UI.Pages {
     export class SettingsPage extends Page {
         private _pluginEngine: Hadouken.Plugins.PluginEngine = Hadouken.Plugins.PluginEngine.getInstance();
         private _rpcClient: Hadouken.Http.JsonRpcClient = new Hadouken.Http.JsonRpcClient('/jsonrpc');
-        private _sections: { [id: string]: { (): void; }; } = {};
 
         constructor() {
             super('/settings.html', [
                 '/settings/:section:'
             ]);
-
-            this._sections['general'] = () => this.loadGeneral();
-            this._sections['plugins'] = () => this.loadPlugins();
         }
 
         load(...args: any[]): void {
-            if (typeof args[0] !== "undefined" && typeof this._sections[args[0]] !== "undefined") {
-                $('#settings-sidebar-menu > li').removeClass('active');
-                $('#settings-menu-' + args[0]).parent().addClass('active');
-
-                var sectionCallback = this._sections[args[0]];
-                sectionCallback();
-            }
+            this.loadGeneral();
+            this.loadPlugins();
         }
 
         loadGeneral(): void {
-            console.log('general page should load');
         }
 
         loadPlugins(): void {
-            $.get('/settings-plugins.html', (html) => {
-                $('#settings-section-container').empty();
-                $('#settings-section-container').append($(html));
+            var template = Handlebars.compile($('#tmpl-plugin-list-item').html());
 
-                var template = Handlebars.compile($('#tmpl-plugin-list-item').html());
+            var data = [['plugins.repositoryUrl', 'plugins.enableUpdateChecking']];
 
-                var data = [['plugins.repositoryUrl', 'plugins.enableUpdateChecking']];
+            this._rpcClient.callParams('config.getMany', data, (response) => {
+                var repositoryUrl = response['plugins.repositoryUrl'];
+                var enableUpdateCheckingVal = response['plugins.enableUpdateChecking'];
+                var enableUpdateChecking = false;
 
-                this._rpcClient.callParams('config.getMany', data, (response) => {
-                    var repositoryUrl = response['plugins.repositoryUrl'];
-                    var enableUpdateCheckingVal = response['plugins.enableUpdateChecking'];
-                    var enableUpdateChecking = false;
+                if (enableUpdateCheckingVal !== null)
+                    enableUpdateChecking = enableUpdateCheckingVal;
 
-                    if (enableUpdateCheckingVal !== null)
-                        enableUpdateChecking = enableUpdateCheckingVal;
+                $('#repositoryUrl').val(repositoryUrl);
+                $('#enableUpdateChecking').attr('checked', enableUpdateChecking);
 
-                    $('#repositoryUrl').val(repositoryUrl);
-                    $('#enableUpdateChecking').attr('checked', enableUpdateChecking);
+                $('#btn-save-plugin-settings').on('click', (e) => {
+                    e.preventDefault();
+                    this.savePluginSettings();
+                });
 
-                    $('#btn-save-plugin-settings').on('click', (e) => {
-                        e.preventDefault();
-                        this.savePluginSettings();
-                    });
+                var pluginIds = Object.keys(this._pluginEngine.plugins);
 
-                    var pluginIds = Object.keys(this._pluginEngine.plugins);
+                for (var i = 0; i < pluginIds.length; i++) {
+                    var key = pluginIds[i];
+                    var plugin = this._pluginEngine.plugins[key];
 
-                    for (var i = 0; i < pluginIds.length; i++) {
-                        var key = pluginIds[i];
-                        var plugin = this._pluginEngine.plugins[key];
+                    var row = template({ plugin: plugin });
+                    $('#tbody-plugin-list').append($(row));
+                }
 
-                        var row = template({ plugin: plugin });
-                        $('#tbody-plugin-list').append($(row));
-                    }
+                var that = this;
 
-                    var that = this;
+                $('.btn-configure-plugin').on('click', function (e) {
+                    e.preventDefault();
 
-                    $('.btn-configure-plugin').on('click', function (e) {
-                        e.preventDefault();
-
-                        var pluginId = $(this).closest("tr").attr('data-plugin-id');
-                        that._pluginEngine.plugins[pluginId].instance.configure();
-                    });
+                    var pluginId = $(this).closest("tr").attr('data-plugin-id');
+                    that._pluginEngine.plugins[pluginId].instance.configure();
                 });
             });
         }
