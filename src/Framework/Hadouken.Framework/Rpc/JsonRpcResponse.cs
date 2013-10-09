299a5fcae9e3828f920d75075433f60cad33578f
Viktor Elofsson <viktor@viktorelofsson.se>
2013-10-09 19:51:18 +0200

    Moved SemanticVersionConverter to a separate file.

--- src/Framework/Hadouken.Framework/Rpc/JsonRpcResponse.cs
+++ src/Framework/Hadouken.Framework/Rpc/JsonRpcResponse.cs
@@ -1,55 +1,10 @@
 ﻿using System;
-using Hadouken.Framework.SemVer;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 using Newtonsoft.Json.Serialization;
 
 namespace Hadouken.Framework.Rpc
 {
-    public class SemanticVersionConverter : JsonConverter
-    {
-        public override bool CanConvert(Type objectType)
-        {
-            return objectType == typeof (SemanticVersion);
-        }
-
-        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
-        {
-            if (reader.TokenType == JsonToken.Null)
-                return null;
-
-            if (reader.TokenType != JsonToken.String)
-                throw new Exception(
-                    String.Format("Unexpected token or value when parsing version. Token: {0}, Value: {1}",
-                        reader.TokenType, reader.Value));
-            try
-            {
-                var semver = new SemanticVersion((string) reader.Value);
-                return semver;
-            }
-            catch (Exception e)
-            {
-                throw new Exception(String.Format("Error parsing SemanticVersion string {0}", reader.Value), e);
-            }
-        }
-
-        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
-        {
-            if (value == null)
-            {
-                writer.WriteNull();
-            }
-            else if (value is SemanticVersion)
-            {
-                writer.WriteValue(value.ToString());
-            }
-            else
-            {
-                throw new Exception("Expected SemanticVersion object value.");
-            }
-        }
-    }
-
     public sealed class JsonRpcResponse
     {
         private static readonly JsonSerializerSettings SerializerSettings = new JsonSerializerSettings();
