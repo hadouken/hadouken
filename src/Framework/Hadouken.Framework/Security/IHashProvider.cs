1632f34bbabdb71d0f318a3ee9c439d3a1e36fc0
Viktor Elofsson <viktor@viktorelofsson.se>
2013-10-11 12:36:50 +0200

    Added an IHashProvider.

---
+++ src/Framework/Hadouken.Framework/Security/IHashProvider.cs
@@ -0,0 +1,9 @@
+﻿namespace Hadouken.Framework.Security
+{
+    public interface IHashProvider
+    {
+        string ComputeHash(string input);
+
+        byte[] ComputeHash(byte[] input);
+    }
+}
---
+++ src/Framework/Hadouken.Framework/Security/Sha256HashProvider.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Hadouken.Framework.Security
+{
+    public class Sha256HashProvider : IHashProvider
+    {
+        public string ComputeHash(string input)
+        {
+            var data = Encoding.UTF8.GetBytes(input);
+            var hash = ComputeHash(data);
+            var sb = new StringBuilder();
+
+            foreach (var b in hash)
+            {
+                sb.AppendFormat("{0:x2}", b);
+            }
+
+            return sb.ToString();
+        }
+
+        public byte[] ComputeHash(byte[] input)
+        {
+            var sha256 = new SHA256Managed();
+            return sha256.ComputeHash(input);
+        }
+    }
+}
