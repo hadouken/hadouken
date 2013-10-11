01f3d9837c20f22bc6361e1fe1b2610fdaff8e98
Viktor Elofsson <viktor@viktorelofsson.se>
2013-10-11 10:36:20 +0200

    Error checking to avoid NaN.

--- src/Plugins/WebUI/Hadouken.Plugins.WebUI/UI/Scripts/Main.ts
+++ src/Plugins/WebUI/Hadouken.Plugins.WebUI/UI/Scripts/Main.ts
@@ -12,6 +12,9 @@
 $(document).ready(function () {
     // Register Handlebars templates
     Handlebars.registerHelper('fileSize', function (size) {
+        if (size < 0)
+            return '';
+
         var i = Math.floor(Math.log(size) / Math.log(1024));
         return (size / Math.pow(1024, i)).toFixed(2) + ' ' + ['B', 'kB', 'MB', 'GB', 'TB'][i];
     });
