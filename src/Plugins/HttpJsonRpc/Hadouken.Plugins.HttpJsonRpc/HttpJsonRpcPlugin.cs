781ed7966e07f06ddb739f242e20d8121f0e791e
Viktor Elofsson <viktor@viktorelofsson.se>
2013-10-11 10:44:16 +0200

    Added base for new HttpJsonRpc plugin.

---
+++ src/Plugins/HttpJsonRpc/Hadouken.Plugins.HttpJsonRpc/HttpJsonRpcPlugin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Hadouken.Framework.Plugins;
+
+namespace Hadouken.Plugins.HttpJsonRpc
+{
+    public class HttpJsonRpcPlugin : Plugin
+    {
+        public override void Load()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Plugins/HttpJsonRpc/Hadouken.Plugins.HttpJsonRpc/PluginBootstrapper.cs
@@ -0,0 +1,13 @@
+﻿using Hadouken.Framework;
+using Hadouken.Framework.Plugins;
+
+namespace Hadouken.Plugins.HttpJsonRpc
+{
+    public class PluginBootstrapper : Bootstrapper
+    {
+        public override Plugin Load(IBootConfig config)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
