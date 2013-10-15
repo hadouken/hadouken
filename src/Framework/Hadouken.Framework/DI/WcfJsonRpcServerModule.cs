84da36137761efe0db494e197fa6f179c0a2fdd5
Viktor Elofsson <viktor@viktorelofsson.se>
2013-10-15 15:49:15 +0200

    ConfigBootstrapper now uses the new structure for registering components (ie. Autofac modules).

---
+++ src/Framework/Hadouken.Framework/DI/WcfJsonRpcServerModule.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using Autofac.Integration.Wcf;
+using Hadouken.Framework.Rpc.Hosting;
+using Hadouken.Framework.Wcf;
+
+namespace Hadouken.Framework.DI
+{
+    public class WcfJsonRpcServerModule : Module
+    {
+        private readonly Func<IContainer> _containerFactory;
+        private readonly string _bindingUri;
+
+        public WcfJsonRpcServerModule(Func<IContainer> containerFactory, string bindingUri)
+        {
+            _containerFactory = containerFactory;
+            _bindingUri = bindingUri;
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<BindingBuilder>().As<IBindingBuilder>().SingleInstance();
+
+            builder.Register<IWcfJsonRpcServer>(c =>
+            {
+                var bindingBuilder = c.Resolve<IBindingBuilder>();
+                var binding = bindingBuilder.Build(_bindingUri);
+
+                var host = new ServiceHost(typeof (WcfJsonRpcService));
+                host.AddServiceEndpoint(typeof (IWcfRpcService), binding, _bindingUri);
+                host.AddDependencyInjectionBehavior<IWcfRpcService>(_containerFactory());
+
+                return new WcfJsonRpcServer(host);
+            });
+        }
+    }
+}
