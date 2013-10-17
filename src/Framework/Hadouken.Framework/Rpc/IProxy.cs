9dc34b22607bff01af780d7d1d3b0fce33af640c
Viktor Elofsson <viktor@viktorelofsson.se>
2013-10-17 21:29:10 +0200

    WCF proxy stuff.

---
+++ src/Framework/Hadouken.Framework/Rpc/IProxy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hadouken.Framework.Rpc
+{
+    public interface IProxy<out T>
+    {
+        T Channel { get; }
+    }
+}
---
+++ src/Framework/Hadouken.Framework/Wcf/Proxy.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using Hadouken.Framework.Rpc;
+
+namespace Hadouken.Framework.Wcf
+{
+    public class Proxy<T> : IProxy<T>
+    {
+        private readonly Uri _endpoint;
+        private readonly IBindingFactory _bindingFactory;
+        private readonly Lazy<T> _lazyChannel;
+
+        public Proxy(Uri endpoint, IBindingFactory bindingFactory)
+        {
+            _endpoint = endpoint;
+            _bindingFactory = bindingFactory;
+            _lazyChannel = new Lazy<T>(BuildChannel);
+        }
+
+        private T BuildChannel()
+        {
+            var binding = _bindingFactory.Create(_endpoint);
+            var factory = new ChannelFactory<T>(binding, _endpoint.ToString());
+
+            return factory.CreateChannel();
+        }
+
+        public T Channel
+        {
+            get { return _lazyChannel.Value; }
+        }
+    }
+}
