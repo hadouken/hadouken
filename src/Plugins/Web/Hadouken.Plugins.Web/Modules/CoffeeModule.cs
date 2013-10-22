4ae5a451262336be26f705dc193f2026b6811261
Viktor Elofsson <viktor@viktorelofsson.se>
2013-10-22 17:27:10 +0200

    Basic CoffeeScript compiling now works.

---
+++ src/Plugins/Web/Hadouken.Plugins.Web/Modules/CoffeeModule.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using Hadouken.Plugins.Web.CoffeeScript;
+using Nancy;
+
+namespace Hadouken.Plugins.Web.Modules
+{
+    public class CoffeeModule : NancyModule
+    {
+        private readonly IRootPathProvider _pathProvider;
+        private readonly ICoffeeCompiler _coffeeCompiler;
+
+        public CoffeeModule(IRootPathProvider pathProvider, ICoffeeCompiler coffeeCompiler)
+        {
+            _pathProvider = pathProvider;
+            _coffeeCompiler = coffeeCompiler;
+
+            Get[@"^(?<path>.*js)$"] = _ => Compile(_.path.ToString());
+        }
+
+        private dynamic Compile(string path)
+        {
+            var coffeeScript = path.Substring(0, path.Length - 3) + ".coffee";
+            coffeeScript = Path.Combine(_pathProvider.GetRootPath(), "UI", coffeeScript);
+
+            if (!File.Exists(coffeeScript))
+                return Response.AsFile("UI/" + path);
+
+            var content = File.ReadAllText(coffeeScript);
+            var compiledContent = _coffeeCompiler.Compile(content);
+
+            return Response.AsText(compiledContent, "text/javascript");
+        }
+    }
+}
---
+++ src/Plugins/Web/Hadouken.Plugins.Web/UI/Scripts/test.coffee
@@ -0,0 +1,3 @@
+﻿class Animal
+  constructor: (name) ->
+    @name = name
