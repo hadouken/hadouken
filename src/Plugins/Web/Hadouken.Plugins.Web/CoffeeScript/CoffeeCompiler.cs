e3b16890ef316af39d231c84c75031b4baa4aa25
Viktor Elofsson <viktor@viktorelofsson.se>
2013-10-22 21:01:51 +0200

    Moved types into their own files.

---
+++ src/Plugins/Web/Hadouken.Plugins.Web/CoffeeScript/CoffeeCompiler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Jurassic;
+
+namespace Hadouken.Plugins.Web.CoffeeScript
+{
+    public class CoffeeCompiler : ICoffeeCompiler
+    {
+        private const string CoffeeScriptCompile = "CoffeeScript.compile(Source, {{bare: {0}}})";
+        private readonly Lazy<ScriptEngine> _scriptEngine;
+
+        public CoffeeCompiler()
+        {
+            _scriptEngine = new Lazy<ScriptEngine>(InitScriptEngine);
+        }
+
+        public string Compile(string source)
+        {
+            _scriptEngine.Value.SetGlobalValue("Source", source);
+            return _scriptEngine.Value.Evaluate<string>(String.Format(CoffeeScriptCompile, "false"));
+        }
+
+        private ScriptEngine InitScriptEngine()
+        {
+            var engine = new ScriptEngine();
+            var compilerScript =
+                ResourceReader.Read("Hadouken.Plugins.Web.CoffeeScript.Resources.CoffeeScript.compiler.js");
+
+            engine.Execute(compilerScript);
+
+            return engine;
+        }
+    }
+}
--- src/Plugins/Web/Hadouken.Plugins.Web/CoffeeScript/ICoffeeCompiler.cs
+++ src/Plugins/Web/Hadouken.Plugins.Web/CoffeeScript/ICoffeeCompiler.cs
@@ -1,56 +1,7 @@
-﻿using System;
-using System.IO;
-using Jurassic;
-
-namespace Hadouken.Plugins.Web.CoffeeScript
+﻿namespace Hadouken.Plugins.Web.CoffeeScript
 {
     public interface ICoffeeCompiler
     {
         string Compile(string source);
-    }
-
-    public class CoffeeCompiler : ICoffeeCompiler
-    {
-        private const string CoffeeScriptCompile = "CoffeeScript.compile(Source, {{bare: {0}}})";
-        private readonly Lazy<ScriptEngine> _scriptEngine;
-
-        public CoffeeCompiler()
-        {
-            _scriptEngine = new Lazy<ScriptEngine>(InitScriptEngine);
-        }
-
-        public string Compile(string source)
-        {
-            _scriptEngine.Value.SetGlobalValue("Source", source);
-            return _scriptEngine.Value.Evaluate<string>(String.Format(CoffeeScriptCompile, "false"));
-        }
-
-        private ScriptEngine InitScriptEngine()
-        {
-            var engine = new ScriptEngine();
-            var compilerScript =
-                ResourceReader.Read("Hadouken.Plugins.Web.CoffeeScript.Resources.CoffeeScript.compiler.js");
-
-            engine.Execute(compilerScript);
-
-            return engine;
-        }
-    }
-
-    public static class ResourceReader
-    {
-        public static string Read(string resourceName)
-        {
-            using (var resource = typeof (ResourceReader).Assembly.GetManifestResourceStream(resourceName))
-            {
-                if (resource == null)
-                    return null;
-                
-                using (var reader = new StreamReader(resource))
-                {
-                    return reader.ReadToEnd();
-                }
-            }
-        }
     }
 }
