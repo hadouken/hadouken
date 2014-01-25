eba2a3f59da0d23921855ff69e46bff87fdc4d52
Viktor Elofsson <viktor@viktorelofsson.se>
2014-01-26 00:51:31 +0100

    Switched from FAKE to regular MSBuild.

--- build.bat
+++ build.bat
@@ -1,28 +1,3 @@
 @echo off
-
-:Build
-cls
-
-if not exist tools\FAKE.Core\tools\Fake.exe ( 
-	echo Installing FAKE...
-	"tools\nuget\nuget.exe" "install" "FAKE.Core" "-OutputDirectory" "tools" "-ExcludeVersion" "-Prerelease"
-	echo.
-)
-
-SET TARGET="All"
-IF NOT [%1]==[] (set TARGET="%1")
-SET BUILDMODE="Release"
-IF NOT [%2]==[] (set BUILDMODE="%2")
-
-echo Starting FAKE...
-"tools\FAKE.Core\tools\Fake.exe" "build.fsx" "target=%TARGET%" "buildMode=%BUILDMODE%"
-
-rem Loop the build script.
-echo.
-set CHOICE=nothing
-echo (R)ebuild or (Enter) to exit
-set /P CHOICE= 
-if /i "%CHOICE%"=="R" goto :Build
-
-:Quit
-exit /b %errorlevel%
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe Hadouken.msbuild
+@pause
