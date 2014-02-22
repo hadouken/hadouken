bd4909d1064ee41d734cb253f497803f1a7c40b4
Viktor Elofsson <viktor@viktorelofsson.se>
2014-02-22 22:45:15 +0100

    Removed the web plugin

--- src/Plugins/Web/Hadouken.Plugins.Web/AuthChangedEventArgs.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Hadouken.Plugins.Web
-{
-    public class AuthChangedEventArgs
-    {
-        public string UserName { get; set; }
-
-        public string HashedPassword { get; set; }
-    }
-}
