b2807e9633dd5498854560e057b603b980bd0b93
Viktor Elofsson <viktor@viktorelofsson.se>
2014-02-22 20:09:00 +0100

    Removed the HttpJsonRpc plugin

--- src/Plugins/HttpJsonRpc/Hadouken.Plugins.HttpJsonRpc/AuthChangedEventArgs.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Hadouken.Plugins.HttpJsonRpc
-{
-    public class AuthChangedEventArgs
-    {
-        public string UserName { get; set; }
-
-        public string HashedPassword { get; set; }
-    }
-}
