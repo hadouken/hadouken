1abac68d61efe1cc437980d900c09cef1d391afe
Viktor Elofsson <viktor@viktorelofsson.se>
2014-02-24 23:14:44 +0100

    Began working on the settings page

---
+++ src/Main/Hadouken/Http/Management/Models/GetSettingsItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Hadouken.Http.Management.Models
+{
+    public class GetSettingsItem
+    {
+        public string InstanceName { get; set; }
+
+        public string DataPath { get; set; }
+
+        public string HttpHostBinding { get; set; }
+
+        public int HttpPort { get; set; }
+
+        public Uri RpcGatewayUri { get; set; }
+
+        public string RpcPluginUriTemplate { get; set; }
+
+        public string PluginsBaseDirectory { get; set; }
+
+        public Uri PluginsRepositoryUri { get; set; }
+    }
+}
---
+++ src/Main/Hadouken/Http/Management/Modules/SettingsModule.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Hadouken.Configuration;
+using Hadouken.Http.Management.Models;
+using Nancy;
+
+namespace Hadouken.Http.Management.Modules
+{
+    public class SettingsModule : NancyModule
+    {
+        public SettingsModule(IConfiguration configuration)
+            : base("settings")
+        {
+            Get["/"] = _ =>
+            {
+                var dto = new GetSettingsItem
+                {
+                    DataPath = configuration.ApplicationDataPath,
+                    HttpHostBinding = configuration.Http.HostBinding,
+                    HttpPort = configuration.Http.Port,
+                    InstanceName = configuration.InstanceName,
+                    PluginsBaseDirectory = configuration.Plugins.BaseDirectory,
+                    PluginsRepositoryUri = configuration.Plugins.RepositoryUri,
+                    RpcGatewayUri = new Uri(configuration.Rpc.GatewayUri),
+                    RpcPluginUriTemplate = configuration.Rpc.PluginUriTemplate
+                };
+
+                return View["Index", dto];
+            };
+        }
+    }
+}
