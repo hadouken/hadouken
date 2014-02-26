0c62d7bcbf5456e105c719c08a20858d9b1d707d
Viktor Elofsson <viktor@viktorelofsson.se>
2014-02-26 02:07:10 +0100

    Updated xmldoc

--- src/Main/Hadouken/Plugins/IPluginEngine.cs
+++ src/Main/Hadouken/Plugins/IPluginEngine.cs
@@ -4,8 +4,15 @@ namespace Hadouken.Plugins
 {
     public interface IPluginEngine
     {
+        /// <summary>
+        /// Gets all <see cref="IPluginManager"/> that are associated with this plugin engine.
+        /// </summary>
         IEnumerable<IPluginManager> GetAll();
 
+        /// <summary>
+        /// Get a specific <see cref="IPluginManager"/> or null if it was not found.
+        /// </summary>
+        /// <param name="name">The name of the <see cref="IPluginManager"/> to get.</param>
         IPluginManager Get(string name);
 
         /// <summary>
@@ -14,7 +21,7 @@ namespace Hadouken.Plugins
         void Scan();
 
         /// <summary>
-        /// Load all plugins which are in the 'Unloaded' state.
+        /// Loads all available <see cref="IPluginManager"/> which are in the 'Unloaded' state.
         /// </summary>
         void LoadAll();
 
@@ -29,6 +36,12 @@ namespace Hadouken.Plugins
         /// <param name="name">The plugin to load.</param>
         void Load(string name);
 
+        /// <summary>
+        /// Check if a specific <see cref="IPluginManager"/> have all dependencies available for it to load.
+        /// </summary>
+        /// <param name="name">The name of the <see cref="IPluginManager"/></param>
+        /// <param name="missingDependencies">A list of the known dependencies which are missing</param>
+        /// <returns>True if the <see cref="IPluginManager"/> can load. Otherwise false.</returns>
         bool CanLoad(string name, out string[] missingDependencies);
 
         /// <summary>
@@ -37,6 +50,13 @@ namespace Hadouken.Plugins
         /// <param name="name">The plugin to unload.</param>
         void Unload(string name);
 
+        /// <summary>
+        /// Installs or upgrades the provided package. If the package upgrades an
+        /// existing plugin, the plugin will be unloaded and then uninstalled before
+        /// the new version.
+        /// </summary>
+        /// <param name="package">The <see cref="IPackage"/> to install or upgrade.</param>
+        /// <returns>True if the install/upgrade was successful. Otherwise false.</returns>
         bool InstallOrUpgrade(IPackage package);
 
         /// <summary>
