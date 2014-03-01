7b730f863ebea21577f520758a9bca5d778cb913
Viktor Elofsson <viktor@viktorelofsson.se>
2014-03-01 18:48:46 +0100

    Management UI now shows the log

--- src/Main/Hadouken/Http/Management/Modules/ToolsModule.cs
+++ src/Main/Hadouken/Http/Management/Modules/ToolsModule.cs
@@ -1,13 +1,17 @@
 ﻿using System.Linq;
+using System.Text;
+using Hadouken.Fx.IO;
 using Hadouken.Plugins;
 using Nancy;
 using Nancy.Security;
+using NLog;
+using NLog.Targets;
 
 namespace Hadouken.Http.Management.Modules
 {
     public class ToolsModule : NancyModule
     {
-        public ToolsModule(IPluginEngine pluginEngine)
+        public ToolsModule(IPluginEngine pluginEngine, IFileSystem fileSystem)
             : base("tools")
         {
             this.RequiresAuthentication();
@@ -16,6 +20,26 @@ namespace Hadouken.Http.Management.Modules
             {
                 var pluginIds = pluginEngine.GetAll().Select(p => p.Manifest.Name).ToArray();
                 return View["JsonRpcDebugger", new {Plugins = pluginIds}];
+            };
+
+            Get["/log"] = _ =>
+            {
+                var sb = new StringBuilder();
+
+                var target = LogManager.Configuration.AllTargets.FirstOrDefault(t => t.Name == "memory");
+                if (target != null)
+                {
+                    var memoryTarget = target as MemoryTarget;
+                    if (memoryTarget != null)
+                    {
+                        foreach (var row in memoryTarget.Logs)
+                        {
+                            sb.AppendLine(row);
+                        }
+                    }
+                }
+
+                return View["Log", new {Log = sb.ToString()}];
             };
         }
     }
