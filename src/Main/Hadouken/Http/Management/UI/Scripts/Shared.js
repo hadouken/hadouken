5b31d66dc2d36a4c8499502ed25e513fbf89339a
Viktor Elofsson <viktor@viktorelofsson.se>
2014-03-01 19:21:30 +0100

    UI now shows a loader icon when an ajax request is in progress

--- src/Main/Hadouken/Http/Management/UI/Scripts/Shared.js
+++ src/Main/Hadouken/Http/Management/UI/Scripts/Shared.js
@@ -1,4 +1,18 @@
 ﻿$(document).ready(function () {
+    $('#ajaxloader').hide();
+    var timeout;
+
+    $.ajaxSetup({
+        beforeSend: function () {
+            $('#ajaxloader').show();
+        },
+        complete: function() {
+            clearTimeout(timeout);
+            timeout = setTimeout(function () { $('#ajaxloader').hide(); }, 500);
+        }
+    });
+
+
     rpc('core', 'core.plugins.list', null, function (result) {
         var container = $('#settings-items-list');
         var plugins = result.result;
