4eed5a50dc7c155b04f93572023769c9fc6b3a2d
Viktor Elofsson <viktor@viktorelofsson.se>
2014-03-01 17:36:56 +0100

    Only plugins which have settings are shown in the settings dropdown

--- src/Main/Hadouken/Http/Management/UI/Scripts/Shared.js
+++ src/Main/Hadouken/Http/Management/UI/Scripts/Shared.js
@@ -1,15 +1,19 @@
 ﻿$(document).ready(function () {
     rpc('core', 'core.plugins.list', null, function (result) {
         var container = $('#settings-items-list');
-
         var plugins = result.result;
 
         for (var i = 0; i < plugins.length; i++) {
-            var item = $('<li><a href="#"></a></li>');
-            item.find('a').attr('href', '/manage/plugins/settings/' + plugins[i]);
-            item.find('a').text(plugins[i]);
 
-            container.append(item);
+            (function(plugin) {
+                $.getJSON('/manage/plugins/settings/' + plugin + '/get', function () {
+                    var item = $('<li><a href="#"></a></li>');
+                    item.find('a').attr('href', '/manage/plugins/settings/' + plugin);
+                    item.find('a').text(plugin);
+
+                    container.append(item);
+                });
+            })(plugins[i]);
         }
     });
 });
