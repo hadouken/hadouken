f181fd97d5930c6643c37a51b985cf4a103d4cbf
Viktor Elofsson <viktor@viktorelofsson.se>
2014-03-04 22:00:17 +0100

    ApiConnection now adds the version of Hadouken to the X-Hadouken-Version header

--- src/Main/Hadouken/Http/ApiConnection.cs
+++ src/Main/Hadouken/Http/ApiConnection.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Net;
 using System.Net.Http;
+using System.Reflection;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 
@@ -8,10 +9,30 @@ namespace Hadouken.Http
 {
     public class ApiConnection : IApiConnection
     {
+        private static readonly string VersionHeaderKey = "X-Hadouken-Version";
+        private readonly string _version;
+
+        public ApiConnection()
+        {
+            var type = typeof (ApiConnection);
+            var infoVersion = type.Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>();
+
+            if (infoVersion != null)
+            {
+                _version = infoVersion.InformationalVersion;
+            }
+        }
+
         public async Task<T> GetAsync<T>(Uri uri) where T : class
         {
             using (var client = new HttpClient())
             {
+                if (string.IsNullOrEmpty(_version))
+                {
+                    // Add the Hadouken version to headers
+                    client.DefaultRequestHeaders.Add(VersionHeaderKey, _version);
+                }
+
                 var response = await client.GetAsync(uri);
                 
                 if(!response.IsSuccessStatusCode)
