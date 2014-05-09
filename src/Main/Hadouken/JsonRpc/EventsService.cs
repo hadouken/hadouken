ad1642d5b886857ef9c5afe5e7e426daa33b7a43
Viktor Elofsson <viktor@viktorelofsson.se>
2014-05-09 16:41:21 +0200

    Basic first version of new explicit event model.

--- src/Main/Hadouken/JsonRpc/EventsService.cs
+++ src/Main/Hadouken/JsonRpc/EventsService.cs
@@ -1,13 +1,55 @@
-﻿using Hadouken.Fx.JsonRpc;
+﻿using System;
+using System.Linq;
+using Hadouken.Fx.JsonRpc;
+using Hadouken.Plugins;
 using Microsoft.AspNet.SignalR;
+using NLog;
 
 namespace Hadouken.JsonRpc
 {
     public class EventsService : IJsonRpcService
     {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+        private const string All = "<all>";
+        private readonly IPluginEngine _pluginEngine;
+        private readonly IJsonRpcClient _rpcClient;
+
+        public EventsService(IPluginEngine pluginEngine, IJsonRpcClient rpcClient)
+        {
+            _pluginEngine = pluginEngine;
+            _rpcClient = rpcClient;
+        }
+
         [JsonRpcMethod("core.events.publish")]
         public void PublishEvent(string name, object data)
         {
+            // Get all plugins with event handlers for the current event
+            // or for <all> events.
+            var handlers = _pluginEngine.GetAll()
+                .SelectMany(
+                    p => p.Manifest.EventHandlers.Where(e => e.Name == name || e.Name == All));
+
+            foreach (var handler in handlers)
+            {
+                // These are the handlers for our event
+                try
+                {
+                    if (handler.Name == All)
+                    {
+                        _rpcClient.Call(handler.Target, new[] {name, data});
+                    }
+                    else
+                    {
+                        _rpcClient.Call(handler.Target, new[] { data });
+                    }
+                }
+                catch (Exception e)
+                {
+                    Logger.ErrorException("Error when calling event handler: " + handler, e);
+                }
+            }
+
+            // To keep legacy plugins from not receiving events.
             var hub = GlobalHost.ConnectionManager.GetHubContext("events");
             hub.Clients.All.publishEvent(new {name, data});
         }
---
+++ src/SamplePlugin/Hadouken.Plugins.Sample/JsonRpc/EventHandlersService.cs
@@ -0,0 +1,27 @@
+﻿using Hadouken.Fx.JsonRpc;
+using Hadouken.Fx.Logging;
+
+namespace Hadouken.Plugins.Sample.JsonRpc
+{
+    public sealed class EventHandlersService : IJsonRpcService
+    {
+        private readonly ILogger _logger;
+
+        public EventHandlersService(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        [JsonRpcMethod("sample.on.customEvent")]
+        public void OnCustomEvent(string foo)
+        {
+            _logger.Info("Custom event received.");
+        }
+
+        [JsonRpcMethod("sample.on.allEvents")]
+        public void OnAllEvents(string eventName, object data)
+        {
+            _logger.Info("Handler for all events invoked. Event name: " + eventName);
+        }
+    }
+}
