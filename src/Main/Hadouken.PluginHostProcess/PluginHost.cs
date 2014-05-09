463a6e42a088d7eec677c2cfb7c551642fb7483a
Viktor Elofsson <viktor@viktorelofsson.se>
2014-05-09 23:38:31 +0200

    Made some refactorings in the plugin host process.

--- src/Main/Hadouken.PluginHostProcess/PluginHost.cs
+++ src/Main/Hadouken.PluginHostProcess/PluginHost.cs
@@ -1,15 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.IO.MemoryMappedFiles;
 using System.Linq;
 using System.Reflection;
+using System.Threading;
 using System.Web.Script.Serialization;
 
 namespace Hadouken.PluginHostProcess
 {
     public class PluginHost
     {
+        private readonly string _pluginId;
         private const string FxPlugin = "Hadouken.Fx.Plugin";
         private const string FxPluginConfiguration = "Hadouken.Fx.PluginConfiguration";
         private const string FxBootstrapperAttribute = "Hadouken.Fx.Bootstrapping.BootstrapperAttribute";
@@ -17,7 +19,12 @@ namespace Hadouken.PluginHostProcess
 
         private object _pluginHost;
 
-        public void Load(string id)
+        public PluginHost(string pluginId)
+        {
+            _pluginId = pluginId;
+        }
+
+        public void Load()
         {
             var files = Directory.GetFiles(".", "*.dll");
             foreach (var file in files)
@@ -57,17 +64,42 @@ namespace Hadouken.PluginHostProcess
                 bootstrapper = Activator.CreateInstance(customBootstrapperType);
             }
 
-            var conf = ReadConfig(fxAssembly, id);
-            bootstrapper.Invoke("Initialize", new object[] {conf});
+            var conf = ReadConfig(fxAssembly, _pluginId);
+            bootstrapper.Invoke("Initialize", new [] {conf});
 
             // Use it to get the IPluginHost
             _pluginHost = bootstrapper.Invoke("GetHost");
             _pluginHost.Invoke("Load");
+
+            using (var loadEvent = EventWaitHandle.OpenExisting(_pluginId + ".load"))
+            {
+                loadEvent.Set();
+            }
         }
 
         public void Unload()
         {
             _pluginHost.Invoke("Unload");
+
+            using (var unloadEvent = EventWaitHandle.OpenExisting(_pluginId + ".unload"))
+            {
+                unloadEvent.Set();
+            }
+        }
+
+        public void SetupMonitoring(int parentProcessId)
+        {
+            var parentProcess = Process.GetProcessById(parentProcessId);
+            parentProcess.EnableRaisingEvents = true;
+            parentProcess.Exited += (sender, eventArgs) => Environment.Exit(9);
+        }
+
+        public void WaitForExit()
+        {
+            using (var waitEvent = EventWaitHandle.OpenExisting(_pluginId + ".wait"))
+            {
+                waitEvent.WaitOne();
+            }
         }
 
         private static object ReadConfig(Assembly fxAssembly, string fileName)
@@ -79,6 +111,6 @@ namespace Hadouken.PluginHostProcess
                 var json = reader.ReadString();
                 return new JavaScriptSerializer().Deserialize(json, fxAssembly.GetType(FxPluginConfiguration));
             }
-        } 
+        }
     }
 }
--- src/Main/Hadouken.PluginHostProcess/Program.cs
+++ src/Main/Hadouken.PluginHostProcess/Program.cs
@@ -1,12 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.IO.MemoryMappedFiles;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading;
-using System.Web.Script.Serialization;
 
 namespace Hadouken.PluginHostProcess
 {
@@ -14,41 +6,36 @@ namespace Hadouken.PluginHostProcess
     {
         public static void Main(string[] args)
         {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Usage: Hadouken.PluginHostProcess.exe <id> <parent process id>");
+                Environment.Exit(1);
+            }
+
+            new Program().Run(args);
+        }
+
+        public void Run(string[] args)
+        {
             // Get current process and create handler to close this child process if
             // parent exists. This is to prevent stray child processes.
             var pluginId = args[0];
             var parentId = Convert.ToInt32(args[1]);
 
-            var parentProcess = Process.GetProcessById(parentId);
-            parentProcess.EnableRaisingEvents = true;
-            parentProcess.Exited += (sender, eventArgs) => Environment.Exit(9);
+            var host = new PluginHost(pluginId);
 
-            Console.WriteLine("Creating sandbox");
-            var sandbox = new PluginHost();
+            // Setup host process monitoring. Kill our process
+            // if the parent shuts down.
+            host.SetupMonitoring(parentId);
 
-            Console.WriteLine("Opening load event");
-            using (var loadEvent = EventWaitHandle.OpenExisting(pluginId + ".load"))
-            {
-                Console.WriteLine("Loading sandbox");
-                sandbox.Load(pluginId);
-                loadEvent.Set();
-            }
+            // Load our plugin.
+            host.Load();
 
-            // Wait until we're done here
-            Console.WriteLine("Opening wait event");
-            using (var waitEvent = EventWaitHandle.OpenExisting(pluginId + ".wait"))
-            {
-                waitEvent.WaitOne();
-            }
+            // Wait for the host to set the exit signal.
+            host.WaitForExit();
 
-            // Unload
-            Console.WriteLine("Opening unload event");
-            using (var unloadEvent = EventWaitHandle.OpenExisting(pluginId + ".unload"))
-            {
-                Console.WriteLine("Unloading sandbox");
-                sandbox.Unload();
-                unloadEvent.Set();
-            }
+            // Unload the plugin.
+            host.Unload();
         }
     }
 }
