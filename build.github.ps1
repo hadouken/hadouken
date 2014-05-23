9c4d1ec35ff64279870996495052928f5a4809e5
Viktor Elofsson <viktor@viktorelofsson.se>
2014-05-23 09:37:54 +0200

    Building in Posh.

---
+++ build.github.ps1
@@ -0,0 +1,56 @@
+﻿Add-Type -AssemblyName System.Net.Http
+
+<#
+.SYNOPSIS
+Performs synchronous HTTP communication with the target specified.
+.DESCRIPTION
+Using the System.Net.WebRequest object, creates an HTTP web request 
+populating it with the $content specified and submitting the request 
+to the $target indicated using the incoming $verb.
+.PARAMETER target
+The target URL to communicate with
+.PARAMETER authHeader
+The content of an AUTHORIZATION header to add to the HTTP request.
+Examples: "Basic someEncodedUserPass" or "token someOAuthToken"
+.PARAMETER verb
+The HTTP verb to assign the request.
+.PARAMETER content
+The string content to encode and add to the request body.
+#>
+function New-GitHubRelease() {
+    param(
+        [string] $target,
+        [object] $data,
+	    [string] $token
+    )
+
+    $netAssembly = [Reflection.Assembly]::GetAssembly([System.Net.Configuration.SettingsSection])
+
+    if($netAssembly)
+    {
+        $bindingFlags = [Reflection.BindingFlags] "Static,GetProperty,NonPublic"
+        $settingsType = $netAssembly.GetType("System.Net.Configuration.SettingsSectionInternal")
+
+        $instance = $settingsType.InvokeMember("Section", $bindingFlags, $null, $null, @())
+
+        if($instance)
+        {
+            $bindingFlags = "NonPublic","Instance"
+            $useUnsafeHeaderParsingField = $settingsType.GetField("useUnsafeHeaderParsing", $bindingFlags)
+
+            if($useUnsafeHeaderParsingField)
+            {
+              $useUnsafeHeaderParsingField.SetValue($instance, $true)
+            }
+        }
+    }
+
+    $client = New-Object System.Net.Http.HttpClient
+    $client.DefaultRequestHeaders.Authorization = New-Object System.Net.Http.Headers.AuthenticationHeaderValue("token", $token)
+    $client.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", "Hadouken Deployment Script");
+
+    $json = ConvertTo-Json -InputObject $data
+    $request = New-Object System.Net.Http.StringContent($json)
+    
+    $result = $client.PostAsync($target, $request).Result;	
+}
--- build.ps1
+++ build.ps1
@@ -1,16 +1,27 @@
+# Import scripts
+. .\build.github.ps1
+
+$Root = $PSScriptRoot
+
 Properties {
-    $Root = Split-Path $psake.build_script_file
+    $Dir = @{
+        Artifacts = Join-Path $Root "build"
+        Binaries  = Join-Path $Root "build/bin"
+    }
 
-    $BuildArtifactsDirectory = Join-Path $Root "build"
-    $BinariesDirectory = Join-Path $BuildArtifactsDirectory "bin"
     $Configuration = "Release"
-    $Version = "0.0.0"
+    $DeployToken   = ""
+    $SolutionFile  = "Hadouken.sln"
 
-    $7za       = Join-Path $Root "tools\7za-9.20\7za.exe"
-    $NuGet     = Join-Path $Root "tools\nuget.exe"
-    $WixCandle = Join-Path $Root "packages\WiX.Toolset.3.8.1128.0\tools\wix\candle.exe"
-    $WixLight  = Join-Path $Root "packages\WiX.Toolset.3.8.1128.0\tools\wix\light.exe"
-    $xUnit     = Join-Path $Root "packages\xunit.runners.1.9.2\tools\xunit.console.clr4.x86.exe"
+    $Tools = @{
+        '7za'     = Join-Path $Root "tools\7za-9.20\7za.exe"
+        NuGet     = Join-Path $Root "tools\nuget.exe"
+        WixCandle = Join-Path $Root "packages\WiX.Toolset.3.8.1128.0\tools\wix\candle.exe"
+        WixLight  = Join-Path $Root "packages\WiX.Toolset.3.8.1128.0\tools\wix\light.exe"
+        xUnit     = Join-Path $Root "packages\xunit.runners.1.9.2\tools\xunit.console.clr4.x86.exe"
+    }
+
+    $Version = "0.0.0"
 }
 
 FormatTaskName (("-"*25) + "[{0}]" + ("-"*25))
@@ -20,62 +31,80 @@ Task Default -depends Clean, Compile, Test, Output, Zip, MSI, NuGet
 Task Clean {
     Write-Host "Cleaning and creating build artifacts folder."
 
-    if (Test-Path $BuildArtifactsDirectory)
+    if (Test-Path $Dir.Artifacts)
     {
-        Remove-Item $BuildArtifactsDirectory -Recurse -Force | Out-Null
+        Remove-Item $Dir.Artifacts -Recurse -Force | Out-Null
     }
 
-    New-Item $BuildArtifactsDirectory -ItemType directory | Out-Null
-    New-Item $BinariesDirectory -ItemType directory | Out-Null
+    New-Item $Dir.Artifacts -ItemType directory | Out-Null
+    New-Item $Dir.Binaries  -ItemType directory | Out-Null
 
-    Exec { msbuild "Hadouken.sln" /t:Clean "/p:Configuration=$Configuration" /v:quiet } 
+    Exec { msbuild $SolutionFile /t:Clean "/p:Configuration=$Configuration" /v:quiet } 
 }
 
 Task Compile -depends Clean {
-    Exec { msbuild "Hadouken.sln" /t:Build "/p:Configuration=$Configuration" /v:quiet }
+    Exec { msbuild $SolutionFile /t:Build "/p:Configuration=$Configuration" /v:quiet }
 }
 
 Task Test -depends Compile {
-    Exec { & $xUnit ".\src\Main\Hadouken.Tests\bin\$Configuration\Hadouken.Tests.dll" }
+    Exec { & $Tools.xUnit ".\src\Main\Hadouken.Tests\bin\$Configuration\Hadouken.Tests.dll" }
 }
 
 Task Output -depends Compile {
-    $source = Join-Path $Root ".\src\Main\Hadouken.Service\bin\$Configuration\*"
+    $source = Join-Path $Root "src\Main\Hadouken.Service\bin\$Configuration\*"
     $filter = ("*.dll", "*.exe")
 
+    Write-Host "Copying files from $source to $($Dir.Binaries)"
+
     # Copy files
-    Get-ChildItem $source -Include $filter | %{Copy-Item -Path $_.FullName -Destination $BinariesDirectory}
+    Get-ChildItem $source -Include $filter | %{Copy-Item -Path $_.FullName -Destination $Dir.Binaries}
 
     # Copy the correct config file
-    Copy-Item -Path .\src\Configuration\Console\Hadouken.Service.exe.config -Destination $BinariesDirectory
+    Copy-Item -Path .\src\Configuration\Console\Hadouken.Service.exe.config -Destination $Dir.Binaries
 }
 
 Task Zip -depends Output {
-    $files = "$BinariesDirectory/*.*"
-    $output = Join-Path $BuildArtifactsDirectory "hdkn-$Version.zip"
+    $files = "$($Dir.Binaries)/*.*"
+    $output = Join-Path $Dir.Artifacts "hdkn-$Version.zip"
 
     Exec {
-        & $7za a -mx=9 $output $files
+        & $Tools.'7za' a -mx=9 $output $files
     }
 }
 
 Task MSI -depends Output {
+    Write-Host "Installing WiX 3.8.1128"
+
+    Exec {
+        & $Tools.NuGet install WiX.Toolset -Version 3.8.1128 -OutputDirectory packages
+    }
+
     $wxs = Get-ChildItem "src\Installer\" -Include *.wxs -recurse | Select-Object FullName | foreach {$_.FullName}
 
     Exec {
-        & $WixCandle "-dBinDir=$BinariesDirectory" "-dBuildVersion=$Version" -dConfigDir=src\Configuration\Service -ext WixUtilExtension -o "$BuildArtifactsDirectory\wixobj\" $wxs
+        & $Tools.WixCandle "-dBinDir=$($Dir.Binaries)" "-dBuildVersion=$Version" -dConfigDir=src\Configuration\Service -ext WixUtilExtension -o "$($Dir.Artifacts)\wixobj\" $wxs
     }
 
-    $wixobj = Get-ChildItem "$BuildArtifactsDirectory\wixobj\" -Include *.wixobj -Recurse | Select-Object FullName | foreach {$_.FullName}
-    $msi = "$BuildArtifactsDirectory\hdkn-$Version.msi"
+    $wixobj = Get-ChildItem "$($Dir.Artifacts)\wixobj\" -Include *.wixobj -Recurse | Select-Object FullName | foreach {$_.FullName}
+    $msi = "$($Dir.Artifacts)\hdkn-$Version.msi"
 
     Exec {
-        & $WixLight -ext WixUIExtension -ext WixUtilExtension -ext WixNetFxExtension -o $msi $wixobj
+        & $Tools.WixLight -ext WixUIExtension -ext WixUtilExtension -ext WixNetFxExtension -o $msi $wixobj
     }
 }
 
 Task NuGet -depends Output {
     Exec {
-        & $NuGet pack Hadouken.SDK.nuspec -Version $Version -NoPackageAnalysis -OutputDirectory $BuildArtifactsDirectory
+        & $Tools.NuGet pack Hadouken.SDK.nuspec -Version $Version -NoPackageAnalysis -OutputDirectory $Dir.Artifacts
     }
+}
+
+Task Publish -depends MSI, Zip, NuGet {
+    $data = @{
+        tag_name         = "v$Version"
+        target_commitish = "master"
+        name             = "Hadouken $Version"
+    }
+
+    New-GitHubRelease "https://api.github.com/repos/hadouken/hadouken/releases" $data $DeployToken
 }
