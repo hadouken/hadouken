f5521d20296fb49a48ed8bbe071e76cc50c7c0b4
Viktor Elofsson <viktor@viktorelofsson.se>
2014-06-09 15:33:05 +0200

    Added log viewer.

--- src/Main/Hadouken/JsonRpc/LogService.cs
+++ src/Main/Hadouken/JsonRpc/LogService.cs
@@ -1,4 +1,7 @@
-﻿using Hadouken.Fx.JsonRpc;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Hadouken.Fx.JsonRpc;
+using Hadouken.Logging;
 using Serilog;
 using Serilog.Events;
 
@@ -7,10 +10,12 @@ namespace Hadouken.JsonRpc
     public class LogService : IJsonRpcService
     {
         private readonly ILogger _logger;
+        private readonly IInMemorySink _memorySink;
 
-        public LogService(ILogger logger)
+        public LogService(ILogger logger, IInMemorySink memorySink)
         {
             _logger = logger;
+            _memorySink = memorySink;
         }
 
         [JsonRpcMethod("core.logging.log")]
@@ -25,6 +30,20 @@ namespace Hadouken.JsonRpc
             _logger.ForContext("PluginId", source).Write(level, message);
 
             return true;
+        }
+
+        [JsonRpcMethod("core.logging.getEntries")]
+        public object[] GetEntries()
+        {
+            return (from ev in _memorySink.LogEvents
+                orderby ev.Timestamp ascending 
+                select new
+                {
+                    ev.Timestamp,
+                    Level = ev.Level.ToString(),
+                    Message = ev.RenderMessage(),
+                    ev.Exception
+                } as object).ToArray();
         }
 
         private LogEventLevel TranslateLogLevel(int level)
--- src/Web/js/modules/tools/controllers/logViewerController.js
+++ src/Web/js/modules/tools/controllers/logViewerController.js
@@ -2,5 +2,10 @@ angular.module('hadouken.tools.controllers.logViewer', [])
     .controller('Tools.LogViewerController', [
         '$scope', 'jsonrpc',
         function ($scope, jsonrpc) {
+            jsonrpc.request('core.logging.getEntries', {
+                success: function(data) {
+                    $scope.entries = data.result;
+                }
+            });
         }
     ]);
