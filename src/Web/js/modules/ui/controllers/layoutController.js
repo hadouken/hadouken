7586b88488ea8a4e3bbc4cefea161c2454584a32
Viktor Elofsson <viktor@viktorelofsson.se>
2014-06-09 14:17:53 +0200

    Added support for adding menu items.

--- src/Web/js/modules/ui/controllers/layoutController.js
+++ src/Web/js/modules/ui/controllers/layoutController.js
@@ -5,10 +5,16 @@
     '$scope', 'messageService',
     function ($scope, messageService) {
         $scope.settingsItems = {};
+        $scope.menuItems = {};
 
         messageService.subscribe('ui.settings.menuItem.add', function(event, params) {
             if ($scope.settingsItems[params.state]) return;
             $scope.settingsItems[params.state] = params;
+        });
+
+        messageService.subscribe('ui.menuItem.add', function(event, params) {
+            if ($scope.menuItems[params.state]) return;
+            $scope.menuItems[params.state] = params;
         });
 
         messageService.publish('ui.onloaded', {});
