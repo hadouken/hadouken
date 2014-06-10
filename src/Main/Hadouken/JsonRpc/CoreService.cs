c3f66965dc52c34f7c0a87958ad4475647181257
Viktor Elofsson <viktor@viktorelofsson.se>
2014-06-10 10:20:05 +0200

    Updated CoreService to return build information.

--- src/Main/Hadouken/JsonRpc/CoreService.cs
+++ src/Main/Hadouken/JsonRpc/CoreService.cs
@@ -1,15 +1,18 @@
-﻿using System.Reflection;
-using Hadouken.Fx.JsonRpc;
+﻿using Hadouken.Fx.JsonRpc;
 
 namespace Hadouken.JsonRpc
 {
     public class CoreService : IJsonRpcService
     {
-        [JsonRpcMethod("core.getVersion")]
-        public string GetVersion()
+        [JsonRpcMethod("core.getInfo")]
+        public object GetInformation()
         {
-            var attribute = typeof (CoreService).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>();
-            return attribute == null ? "0.0.0" : attribute.InformationalVersion;
+            return new
+            {
+                AssemblyInformation.BranchName,
+                AssemblyInformation.BuildDate,
+                AssemblyInformation.Commit
+            };
         }
     }
 }
