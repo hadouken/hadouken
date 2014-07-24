c9dba05937391e78dd329fe529e25c416bb81371
Viktor Elofsson <viktor@viktorelofsson.se>
2014-07-24 18:48:54 +0200

    Added IO and globbing parts courtesy of @patriksvensson.

---
+++ src/Hadouken.Common/IHadoukenEnvironment.cs
@@ -0,0 +1,39 @@
+﻿using Hadouken.Common.IO;
+
+namespace Hadouken.Common
+{
+    public interface IHadoukenEnvironment
+    {
+        /// <summary>
+        /// Gets whether or not the current operative system is 64 bit.
+        /// </summary>
+        /// <returns>Whether or not the current operative system is 64 bit.</returns>
+        bool Is64BitOperativeSystem();
+
+        /// <summary>
+        /// Determines whether the current machine is running Unix.
+        /// </summary>
+        /// <returns>Whether or not the current machine is running Unix.</returns>
+        bool IsUnix();
+
+        /// <summary>
+        /// Gets a special path.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <returns>A <see cref="DirectoryPath"/> to the special path.</returns>
+        DirectoryPath GetSpecialPath(SpecialPath path);
+
+        /// <summary>
+        /// Gets the application root path.
+        /// </summary>
+        /// <returns>The application root path.</returns>
+        DirectoryPath GetApplicationRoot();
+
+        /// <summary>
+        /// Gets an environment variable.
+        /// </summary>
+        /// <param name="variable">The variable.</param>
+        /// <returns>The value of the environment variable.</returns>
+        string GetEnvironmentVariable(string variable);
+    }
+}
