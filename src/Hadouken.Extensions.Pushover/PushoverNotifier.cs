6e567af2d00b5feb486319c3018325e19114dc8d
Viktor Elofsson <viktor@viktorelofsson.se>
2014-07-24 19:04:46 +0200

    Added in the super basic Pushover notifier.

---
+++ src/Hadouken.Extensions.Pushover/PushoverNotifier.cs
@@ -0,0 +1,17 @@
+﻿using Hadouken.Common.Extensibility;
+using Hadouken.Common.Extensibility.Notifications;
+
+namespace Hadouken.Extensions.Pushover
+{
+    [Extension("notifier.pushover",
+        Name = "Pushover",
+        Description = "Sends push notifications to your mobile devices via Pushover."
+    )]
+    public class PushoverNotifier : INotifier
+    {
+        public void Notify(Notification notification)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
