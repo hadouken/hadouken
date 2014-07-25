97da8ac88ac818b3ba8227534e649f52d7dc561a
Viktor Elofsson <viktor@viktorelofsson.se>
2014-07-25 09:25:12 +0200

    Implemented simple config service for the Pushover extension.

---
+++ src/Hadouken.Extensions.Pushover/Services/ConfigService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Hadouken.Common.Data;
+using Hadouken.Common.JsonRpc;
+using Hadouken.Extensions.Pushover.Config;
+
+namespace Hadouken.Extensions.Pushover.Services
+{
+    public sealed class ConfigService : IJsonRpcService
+    {
+        private readonly IKeyValueStore _keyValueStore;
+
+        public ConfigService(IKeyValueStore keyValueStore)
+        {
+            if (keyValueStore == null) throw new ArgumentNullException("keyValueStore");
+            _keyValueStore = keyValueStore;
+        }
+
+        [JsonRpcMethod("pushover.config.get")]
+        public PushoverConfig GetConfig()
+        {
+            return _keyValueStore.Get<PushoverConfig>("pushover.config");
+        }
+
+        [JsonRpcMethod("pushover.config.set")]
+        public void SetConfig(PushoverConfig config)
+        {
+            _keyValueStore.Set("pushover.config", config);
+        }
+    }
+}
