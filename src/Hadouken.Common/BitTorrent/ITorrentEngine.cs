6d81d0ae8734e7ae780e8c9e366d104ed446155c
Viktor Elofsson <viktor@viktorelofsson.se>
2014-09-08 23:45:18 +0200

    Added a few xmldoc comments.

--- src/Hadouken.Common/BitTorrent/ITorrentEngine.cs
+++ src/Hadouken.Common/BitTorrent/ITorrentEngine.cs
@@ -4,8 +4,14 @@ namespace Hadouken.Common.BitTorrent
 {
     public interface ITorrentEngine
     {
+        /// <summary>
+        /// Gets a list of all torrents registered with this <see cref="ITorrentEngine"/> instance.
+        /// </summary>
         IEnumerable<ITorrent> GetAll();
 
+        /// <summary>
+        /// Gets a single <see cref="ITorrent"/> by its InfoHash property.
+        /// </summary>
         ITorrent GetByInfoHash(string infoHash);
     }
 }
