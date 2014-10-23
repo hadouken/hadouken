fefeda494ec40bd6b43d31569cb2e8bbf691cfce
Egor Nazarov <enazarov@gmail.com>
2014-10-24 01:56:18 +0400

    Revert "Remove torrent with files option"
    This reverts commit dbd4b58d286ebf18a1ba16b406b145599fa1ada6.

--- src/Web/js/modules/bittorrent/controllers/torrentListController.js
+++ src/Web/js/modules/bittorrent/controllers/torrentListController.js
@@ -140,15 +140,15 @@
         };
 
         $scope.remove = function(infoHash) {
-            remoteTorrent(infoHash, false);
-        };
+            var index = getIndex(infoHash);
 
-        $scope.removeWithFiles = function (infoHash) {
-            var confirmResult = confirm("Do you want to remove the torrent and all files?");
-            if (confirmResult == true) {
-                removeTorrent(infoHash, true);
-            }
-        }
+            jsonrpc.request('torrents.remove', {
+                params: [infoHash, false],
+                success: function() {}
+            });
+
+            $scope.torrents.splice(index, 1);
+        };
 
         $scope.queuePosUp = function(infoHash) {
             notify('torrents.queue.up', infoHash);
@@ -254,17 +254,6 @@
                 }
             });
         }
-
-        function removeTorrent(infoHash, removeWithFiles) {
-            var index = getIndex(infoHash);
-            jsonrpc.request('torrents.remove', {
-                params: [infoHash, removeWithFiles],
-                success: function() {}
-            });
-
-            $scope.torrents.splice(index, 1);
-        }
-
         var timer = $timeout(update, 0);
 
         $scope.$on('$destroy', function() {
