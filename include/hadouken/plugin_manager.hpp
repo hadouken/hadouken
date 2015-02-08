94f1908fab2f631fb8448a5e0a148ecf9a7c44e4
Viktor Elofsson <viktor@viktorelofsson.se>
2015-02-08 22:54:30 +0100

    Began working on Python plugin interface.

--- include/hadouken/plugin_manager.hpp
+++ include/hadouken/plugin_manager.hpp
@@ -2,14 +2,11 @@
 #define HDKN_PLUGIN_MANAGER_HPP
 
 #include <boost/asio/io_service.hpp>
-#include <hadouken/plugin.hpp>
+#include <hadouken/service_locator.hpp>
 
 namespace hadouken
 {
-    typedef plugin* create_t(hadouken::service_locator&);
-    typedef void destroy_t(plugin*);
-
-    class HDKN_API plugin_manager
+    class __declspec(dllexport) plugin_manager
     {
     public:
         plugin_manager(hadouken::service_locator& service_locator);
@@ -19,12 +16,8 @@ namespace hadouken
         void unload();
 
     private:
-        void* open_dynamic_library(const std::string& file);
-        void close_dynamic_library(void* handle);
-        void* get_library_symbol(void* handle, const std::string& symbol);
-
         hadouken::service_locator& service_locator_;
-        std::map<std::string, hadouken::plugin*> plugins_;
+        std::map<std::string, void*> plugins_;
     };
 }
 
