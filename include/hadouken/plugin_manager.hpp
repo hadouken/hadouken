d340441494420c64579b84dc2780038d1a1ad92a
Viktor Elofsson <viktor@viktorelofsson.se>
2015-02-10 20:09:45 +0100

    Revert "Began working on Python plugin interface."
    This reverts commit 94f1908fab2f631fb8448a5e0a148ecf9a7c44e4.

--- include/hadouken/plugin_manager.hpp
+++ include/hadouken/plugin_manager.hpp
@@ -2,11 +2,14 @@
 #define HDKN_PLUGIN_MANAGER_HPP
 
 #include <boost/asio/io_service.hpp>
-#include <hadouken/service_locator.hpp>
+#include <hadouken/plugin.hpp>
 
 namespace hadouken
 {
-    class __declspec(dllexport) plugin_manager
+    typedef plugin* create_t(hadouken::service_locator&);
+    typedef void destroy_t(plugin*);
+
+    class HDKN_API plugin_manager
     {
     public:
         plugin_manager(hadouken::service_locator& service_locator);
@@ -16,8 +19,12 @@ namespace hadouken
         void unload();
 
     private:
+        void* open_dynamic_library(const std::string& file);
+        void close_dynamic_library(void* handle);
+        void* get_library_symbol(void* handle, const std::string& symbol);
+
         hadouken::service_locator& service_locator_;
-        std::map<std::string, void*> plugins_;
+        std::map<std::string, hadouken::plugin*> plugins_;
     };
 }
 
--- src/plugin_manager.cpp
+++ src/plugin_manager.cpp
@@ -1,58 +1,25 @@
 #include <iostream>
 
+#ifdef WIN32
+    #include <windows.h>
+#else
+    #include <dlfcn.h>
+#endif
+
 #include <boost/asio/io_service.hpp>
 #include <boost/filesystem.hpp>
 #include <boost/log/trivial.hpp>
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/json_parser.hpp>
-#include <boost/python.hpp>
 
 #include <hadouken/plugin_manager.hpp>
+#include <hadouken/plugin.hpp>
 
 using namespace hadouken;
-namespace py = boost::python;
-
-std::string parse_python_exception();
-
-class plugin
-{
-public:
-    virtual void load() = 0;
-
-    virtual void unload() {}
-};
-
-class plugin_wrapper : public plugin
-{
-public:
-    plugin_wrapper(PyObject* p)
-        : self(p)
-    {
-    }
-
-    void load()
-    {
-        py::call_method<void>(self, "load");
-    }
-
-private:
-    PyObject* self;
-};
-
-BOOST_PYTHON_MODULE(hadouken)
-{
-    py::class_<plugin, boost::noncopyable, boost::shared_ptr<plugin_wrapper>>("Plugin")
-        .def("load", &plugin_wrapper::load)
-        ;
-}
 
 plugin_manager::plugin_manager(hadouken::service_locator& service_locator)
     : service_locator_(service_locator)
 {
-    // Initialize Python interpreter but do not
-    // subscribe to any signals.
-    PyImport_AppendInittab("hadouken", &inithadouken);
-    Py_InitializeEx(0);
 }
 
 plugin_manager::~plugin_manager()
@@ -61,85 +28,96 @@ plugin_manager::~plugin_manager()
 
 void plugin_manager::load()
 {
-    py::object main_module = py::import("__main__");
-    py::object global = main_module.attr("__dict__");
+    namespace fs = boost::filesystem;
 
-    try
+    fs::path plugins_config("config/plugins.json");
+    
+    if (!fs::exists(plugins_config))
     {
-        py::exec("import sys\nsys.path.append('C:/temp')\n", global, global);
-        py::object o = py::import("auto_move");
-        py::object f = o.attr(py::str("AutoMove"))();
-        
-        plugin& pl = py::extract<plugin&>(f);
-        pl.load();
-        
-
-        //py::object result = py::exec_file("C:/temp/hadouken.py", global, global);
-        //py::object f = py::getattr(result, "foo");
-
-        plugins_.insert(std::make_pair("hdkn", PyEval_SaveThread()));
-
-        BOOST_LOG_TRIVIAL(debug) << "executed script";
+        BOOST_LOG_TRIVIAL(warning) << "No \"plugins.json\" found at " << fs::current_path() / "config";
+        return;
     }
-    catch (py::error_already_set const &)
+
+    boost::property_tree::ptree pt;
+    boost::property_tree::read_json(plugins_config.string(), pt);
+
+    std::string plugins_directory = pt.get<std::string>("plugins_directory");
+    fs::path plugins_path(plugins_directory);
+
+    if (!fs::exists(plugins_path))
     {
-        BOOST_LOG_TRIVIAL(error) << parse_python_exception();
+        BOOST_LOG_TRIVIAL(warning) << "Plugins directory does not exist.";
+        return;
     }
+
+    boost::property_tree::ptree plugins_list = pt.get_child("plugins");
+
+    for (auto plugin_list_item : plugins_list)
+    {
+        std::string plugin_id = plugin_list_item.second.get<std::string>("");
+        std::string plugin_file = plugin_id + ".dll";
+
+        fs::path plugin_path = plugins_path / plugin_file;
+
+        if (!fs::exists(plugin_path))
+        {
+            BOOST_LOG_TRIVIAL(warning) << "Plugin file " << plugin_path << " does not exist.";
+            continue;
+        }
+
+        BOOST_LOG_TRIVIAL(info) << "Loading plugin " << plugin_id << " from file " << plugin_path;
+
+        // open library and find create symbol
+        void* handle = open_dynamic_library(plugin_path.string());
+        create_t* creator = (create_t*)get_library_symbol(handle, "hdkn_create_" + plugin_id);
+
+        // invoke create and load plugin
+        plugin* plugin = creator(service_locator_);
+        plugin->load();
+
+        plugins_.insert(std::make_pair(plugin_id, plugin));
+    }
+
+    /*
+    destroy_t* destroyer = (destroy_t*)GetProcAddress(lib, "hdkn_destroy_folder_watcher");
+    destroyer(plugin);
+
+    FreeLibrary(lib);
+    */
 }
 
 void plugin_manager::unload()
 {
+    for (auto it : plugins_)
+    {
+        BOOST_LOG_TRIVIAL(info) << "Unloading plugin " << it.first;
+        it.second->unload();
+    }
 }
 
-// Parses the value of the active python exception
-// NOTE SHOULD NOT BE CALLED IF NO EXCEPTION
-std::string parse_python_exception()
+void* plugin_manager::open_dynamic_library(const std::string& file)
 {
-    PyObject *type_ptr = NULL, *value_ptr = NULL, *traceback_ptr = NULL;
-    // Fetch the exception info from the Python C API
-    PyErr_Fetch(&type_ptr, &value_ptr, &traceback_ptr);
+    #ifdef WIN32
+        return LoadLibraryA(file.c_str());
+    #else
+        return dlopen(file.c_str(), RTLD_LAZY | RTLD_GLOBAL);
+    #endif
+}
 
-    // Fallback error
-    std::string ret("Unfetchable Python error");
-    // If the fetch got a type pointer, parse the type into the exception string
-    if (type_ptr != NULL){
-        py::handle<> h_type(type_ptr);
-        py::str type_pstr(h_type);
-        // Extract the string from the boost::python object
-        py::extract<std::string> e_type_pstr(type_pstr);
-        // If a valid string extraction is available, use it 
-        //  otherwise use fallback
-        if (e_type_pstr.check())
-            ret = e_type_pstr();
-        else
-            ret = "Unknown exception type";
-    }
-    // Do the same for the exception value (the stringification of the exception)
-    if (value_ptr != NULL){
-        py::handle<> h_val(value_ptr);
-        py::str a(h_val);
-        py::extract<std::string> returned(a);
-        if (returned.check())
-            ret += ": " + returned();
-        else
-            ret += std::string(": Unparseable Python error: ");
-    }
-    // Parse lines from the traceback using the Python traceback module
-    if (traceback_ptr != NULL){
-        py::handle<> h_tb(traceback_ptr);
-        // Load the traceback module and the format_tb function
-        py::object tb(py::import("traceback"));
-        py::object fmt_tb(tb.attr("format_tb"));
-        // Call format_tb to get a list of traceback strings
-        py::object tb_list(fmt_tb(h_tb));
-        // Join the traceback strings into a single string
-        py::object tb_str(py::str("\n").join(tb_list));
-        // Extract the string, check the extraction, and fallback in necessary
-        py::extract<std::string> returned(tb_str);
-        if (returned.check())
-            ret += ": " + returned();
-        else
-            ret += std::string(": Unparseable Python traceback");
-    }
-    return ret;
+void plugin_manager::close_dynamic_library(void* handle)
+{
+    #ifdef WIN32
+        FreeLibrary((HMODULE)handle);
+    #else
+        dlclose(handle);
+    #endif
+}
+
+void* plugin_manager::get_library_symbol(void* handle, const std::string& symbol)
+{
+    #ifdef WIN32
+        return GetProcAddress((HMODULE)handle, symbol.c_str());
+    #else
+        return dlsym(handle, symbol.c_str());
+    #endif
 }
