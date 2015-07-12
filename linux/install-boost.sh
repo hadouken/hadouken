91aa71bb026b4b44752c0de138a50e2552234dbd
Viktor Elofsson <viktor@viktorelofsson.se>
2015-07-12 21:49:27 +0200

    Build Boost with default toolset.

--- linux/install-boost.sh
+++ linux/install-boost.sh
@@ -4,8 +4,7 @@ set -e
 if [ ! -d "$HOME/boost/lib" ]; then
   wget http://downloads.sourceforge.net/project/boost/boost/1.58.0/boost_1_58_0.tar.gz
   tar -xzvf boost_1_58_0.tar.gz > /dev/null
-  echo "using gcc : 4.9 : /usr/bin/g++-4.9 ; " >> $HOME/user-config.jam
-  cd boost_1_58_0 && ./bootstrap.sh toolset=gcc-4.9 --prefix=$HOME/boost --with-libraries=system,log,filesystem,program_options,thread && ./b2 toolset=gcc-4.9 install && ./b2 install >> /dev/null
+  cd boost_1_58_0 && ./bootstrap.sh --prefix=$HOME/boost --with-libraries=system,log,filesystem,program_options,thread && ./b2 install >> /dev/null
 else
   echo "Using cached directory."
 fi
