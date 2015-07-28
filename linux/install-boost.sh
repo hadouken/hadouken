44ede4ffcdbfb73837c2c81a82dcb7d3385cd4a8
Viktor Elofsson <viktor@viktorelofsson.se>
2015-07-28 16:53:38 -0700

    Precompile libtorrent on travis.

--- linux/install-boost.sh
+++ linux/install-boost.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 set -e
-# check to see if protobuf folder is empty
+# check to see if boost folder is empty
 if [ ! -d "$HOME/boost/lib" ]; then
   wget http://downloads.sourceforge.net/project/boost/boost/1.58.0/boost_1_58_0.tar.gz
   tar -xzvf boost_1_58_0.tar.gz > /dev/null
   echo "using gcc : 4.8 : /usr/bin/g++-4.8 ; " >> $HOME/user-config.jam
   cd boost_1_58_0 && ./bootstrap.sh toolset=gcc-4.8 --prefix=$HOME/boost --with-libraries=system,log,filesystem,program_options,thread && ./b2 toolset=gcc-4.8 install >> /dev/null
 else
-  echo "Using cached directory."
+  echo "Using cached boost directory."
 fi
