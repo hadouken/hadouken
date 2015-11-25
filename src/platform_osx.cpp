68747b28005c4b17ebffb15d01a75c0f449f64a0
alter <p@scene.cl>
2015-11-25 19:14:13 -0300

    Now cmake script uses find_package to find boost libraries Now cmake script accepts -DBOOST_ROOT parameter to specify custom boost instalations
    It should compile on OSX:
    
        mkdir build
        cd build
        cmake .. -DBOOST_ROOT=<path to custom boost instalation>
        make -j 8
    
    the binary is going to be generated in the bin/ subfolder
    
    webui.zip and js folder needs to be copied in the same folder as the
    resulting binary

---
+++ src/platform_osx.cpp
@@ -0,0 +1,52 @@
+#ifdef __APPLE__
+
+#include <boost/filesystem.hpp>
+#include <hadouken/platform.hpp>
+
+#include <cstdlib>
+#include <mach-o/dyld.h>
+
+using namespace hadouken;
+namespace fs = boost::filesystem;
+
+void platform::init() {}
+
+fs::path platform::data_path() 
+{
+    // TODO: find folders using URLsForDirectory
+
+    char *home_path = getenv("HOME");
+
+    if(home_path != nullptr) {
+        std::string p(home_path);
+        p += "/Downloads";
+        return fs::path(p);
+    }
+
+    return "";
+}
+
+fs::path platform::application_path() 
+{
+    char path[2048];
+    uint32_t size = sizeof(path);
+
+    if (_NSGetExecutablePath(path, &size) == 0) {
+        return fs::path(path).parent_path();
+    }
+
+    return "";
+}
+
+fs::path platform::get_current_directory() 
+{ 
+    return fs::initial_path(); 
+}
+
+int platform::launch_process(std::string executable,
+                             std::vector<std::string> args) 
+{
+    return 0;
+}
+
+#endif
